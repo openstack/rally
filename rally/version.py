58cac83701604bae365fa934051a2a02f42855d9
Boris Pavlovic <boris@pavlovic.me>
2013-08-24 00:20:21 +0400

    Add version module
    At this moment this model has only one method,
    get version_string that returns version of project.
    
    It will be used by cli utils

---
+++ rally/version.py
@@ -0,0 +1,29 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2013: Mirantis Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from pbr import version as pbr_version
+
+RALLY_VENDOR = "OpenStack Foundation"
+RALLY_PRODUCT = "OpenStack Rally"
+RALLY_PACKAGE = None  # OS distro package version suffix
+
+loaded = False
+version_info = pbr_version.VersionInfo('rally')
+
+
+def version_string():
+    return version_info.version_string()
