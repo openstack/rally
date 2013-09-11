6ecc94ec7ba95521caa92414ddea6388edf43cd7
Boris Pavlovic <boris@pavlovic.me>
2013-09-11 14:29:15 +0400

    Partial implement fake deploy engine
    This engine is very usefull when we have already deployed OpenStack
    cloud and would like to test it with Rally.
    
    On deploy it just returns cloud config (endpoints users and so on)
    that was specified in task config.
    
    TODO: Add verification for input endpoints
    
    blueprint deploy-engine-fake
    
    Change-Id: I5c139f6b97312b6e4e00913c0a987f0bfbcb203f

---
+++ rally/deploy/engines/fake_engine.py
@@ -0,0 +1,45 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2013: Mirantis Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from rally.deploy import engine
+
+
+class FakeEngine(engine.EngineFactory):
+    """FakeEngine doesn't deploy OpenStack it just use existing.
+
+       To use FakeEngine you should put in task deploy config `cloud_config`:
+       {'deploy': {'cloud_config': {/* here you should specify endpoints */}}}
+
+       E.g.
+       cloud_config: {
+           'identity': {
+               'url': 'http://localhost/',
+               'admin_user': 'amdin'
+               ....
+           }
+       }
+    """
+
+    def __init__(self, config):
+        self.config = config.get('cloud_config', {})
+        # TODO(boris-42): We should verify endpoints here.
+
+    def deploy(self):
+        return self.config
+
+    def cleanup(self):
+        pass
---
+++ tests/deploy/test_fake_engine.py
@@ -0,0 +1,44 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2013: Mirantis Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Test fake deploy engines."""
+
+from rally import deploy
+from rally.deploy.engines import fake_engine
+from rally import test
+
+
+class TestFakeDeployEngine(test.NoDBTestCase):
+
+    def test_fake_egnine_init(self):
+        fake_engine.FakeEngine({})
+
+    def test_fake_engine_init_with_deploy_config(self):
+        cloud_config = {'cloud_config': {'a': 1, 'b': 2}}
+        fake_engine.FakeEngine(cloud_config)
+
+    def test_fake_engine_deploy(self):
+        cloud_config = {'cloud_config': {'a': 1, 'b': 2}}
+        engine = fake_engine.FakeEngine(cloud_config)
+        self.assertEqual(engine.deploy(), cloud_config['cloud_config'])
+
+    def test_fake_engine_cleanup(self):
+        fake_engine.FakeEngine({}).cleanup()
+
+    def test_fake_engine_is_in_factory(self):
+        engine = deploy.EngineFactory.get_engine('FakeEngine', {})
+        self.assertIsInstance(engine, fake_engine.FakeEngine)
