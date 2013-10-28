318bf44e5e75cdff9da633f43ef272ff13312e57
Sergey Skripnick <sskripnick@mirantis.com>
2013-10-28 17:21:43 +0200

    Remove old sshutils functions
    These functions aren't used anymore
    
    blueprint serverdto-ssh
    
    Change-Id: Ie16ce79c07f4029bdb27cf060e2044662a7e845c

--- tests/test_sshutils.py
+++ tests/test_sshutils.py
@@ -21,52 +21,6 @@ from rally import sshutils
 from rally import test
 
 
-class SshutilsTestCase(test.TestCase):
-
-    def setUp(self):
-        super(SshutilsTestCase, self).setUp()
-        self.pipe = mock.MagicMock()
-        self.pipe.communicate = mock.MagicMock(return_value=(mock.MagicMock(),
-                                               mock.MagicMock()))
-        self.pipe.returncode = 0
-        self.new = mock.MagicMock()
-        self.new.PIPE = self.pipe
-        self.new.Popen = mock.MagicMock(return_value=self.pipe)
-
-    def test_upload_file(self):
-        with mock.patch('rally.sshutils.subprocess', new=self.new) as sp:
-            sshutils.upload_file('root', 'example.com', '/tmp/s', '/tmp/d')
-
-        expected = [mock.call.Popen(['scp', '-o',
-                                     'StrictHostKeyChecking=no',
-                                     '/tmp/s', 'root@example.com:/tmp/d'],
-                    stderr=self.pipe),
-                    mock.call.PIPE.communicate()]
-        self.assertEqual(sp.mock_calls, expected)
-
-    def test_execute_script_no_file(self):
-        self.assertRaises(IOError, sshutils.execute_script, 'user', 'host',
-                          '/ioerror')
-
-    def test_execute_script(self):
-        with mock.patch('rally.sshutils.subprocess', new=self.new) as sp:
-            with mock.patch('rally.sshutils.open', create=True) as op:
-                sshutils.execute_script('user', 'example.com', '/tmp/s')
-        expected = [
-            mock.call.check_call(['ssh', '-o', 'StrictHostKeyChecking=no',
-                                  'user@example.com', '/bin/sh'], stdin=op())]
-        self.assertEqual(sp.mock_calls, expected)
-
-    def test_execute_command(self):
-        with mock.patch('rally.sshutils.subprocess', new=self.new) as sp:
-            sshutils.execute_command('user', 'host', ['command', 'arg'])
-        expected = [
-            mock.call.Popen(['ssh', '-o', 'StrictHostKeyChecking=no',
-                             'user@host', 'command', 'arg'], stderr=self.pipe),
-            mock.call.PIPE.communicate()]
-        self.assertEqual(sp.mock_calls, expected)
-
-
 class SSHTestCase(test.TestCase):
 
     def setUp(self):
