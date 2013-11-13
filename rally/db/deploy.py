554a0993e6d88f741774ccf6e382e94015d9e0bc
Ilya Kharin <akscram@gmail.com>
2013-11-13 19:37:14 +0400

    Abstract out DB operations on deploys in db.deploy
    The db.deploy module contains the Deployment class with an abstract
    methods. Those methods provides high levels operations on the
    deployments.
    
    Change-Id: Ibca797832ebbc96247703cdfc8b0958d10a71ddd
    Implements: blueprint independent-deploy

---
+++ rally/db/deploy.py
@@ -0,0 +1,50 @@
+# Copyright 2013: Mirantis Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from rally import db
+
+
+class Deployment(object):
+    """Represents a deployment object."""
+
+    def __init__(self, deployment=None):
+        if deployment:
+            self.deployment = deployment
+        else:
+            self.deployment = db.deployment_create({})
+
+    def __getitem__(self, key):
+        return self.deployment[key]
+
+    @staticmethod
+    def get(uuid):
+        return Deployment(db.deployment_get(uuid))
+
+    def update_status(self, status):
+        db.deployment_update(self.deployment['uuid'], {'status': status})
+
+    def update_name(self, name):
+        db.deployment_update(self.deployment['uuid'], {'name': name})
+
+    def update_config(self, config):
+        db.deployment_update(self.deployment['uuid'], {'config': config})
+
+    def update_endpoint(self, endpoint):
+        db.deployment_update(self.deployment['uuid'], {
+            'endpoint': endpoint,
+        })
+
+    def delete(self):
+        db.deployment_delete(self.deployment['uuid'])
---
+++ tests/db/test_deploy.py
@@ -0,0 +1,99 @@
+# Copyright 2013: Mirantis Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Tests for db.deploy layer."""
+
+import mock
+import uuid
+
+from rally import consts
+from rally.db import deploy as db_deploy
+from rally import test
+
+
+class DeploymentTestCase(test.TestCase):
+    def setUp(self):
+        super(DeploymentTestCase, self).setUp()
+        self.deployment = {
+            'uuid': str(uuid.uuid4()),
+            'name': '',
+            'config': {},
+            'endpoint': {},
+            'status': consts.DeployStatus.DEPLOY_INIT,
+        }
+
+    @mock.patch('rally.db.deploy.db.deployment_create')
+    def test_init_with_create(self, mock_create):
+        mock_create.return_value = self.deployment
+        deploy = db_deploy.Deployment()
+        mock_create.assert_called_once_with({})
+        self.assertEqual(deploy['uuid'], self.deployment['uuid'])
+
+    @mock.patch('rally.db.deploy.db.deployment_create')
+    def test_init_without_create(self, mock_create):
+        deploy = db_deploy.Deployment(deployment=self.deployment)
+        self.assertFalse(mock_create.called)
+        self.assertEqual(deploy['uuid'], self.deployment['uuid'])
+
+    @mock.patch('rally.db.deploy.db.deployment_get')
+    def test_get(self, mock_get):
+        mock_get.return_value = self.deployment
+        deploy = db_deploy.Deployment.get(self.deployment['uuid'])
+        mock_get.assert_called_once_with(self.deployment['uuid'])
+        self.assertEqual(deploy['uuid'], self.deployment['uuid'])
+
+    @mock.patch('rally.db.deploy.db.deployment_delete')
+    @mock.patch('rally.db.deploy.db.deployment_create')
+    def test_create_and_delete(self, mock_create, mock_delete):
+        mock_create.return_value = self.deployment
+        deploy = db_deploy.Deployment()
+        deploy.delete()
+        mock_delete.assert_called_once_with(self.deployment['uuid'])
+
+    @mock.patch('rally.db.deploy.db.deployment_update')
+    def test_update_status(self, mock_update):
+        deploy = db_deploy.Deployment(deployment=self.deployment)
+        deploy.update_status(consts.DeployStatus.DEPLOY_FAILED)
+        mock_update.assert_called_once_with(
+            self.deployment['uuid'],
+            {'status': consts.DeployStatus.DEPLOY_FAILED},
+        )
+
+    @mock.patch('rally.db.deploy.db.deployment_update')
+    def test_update_name(self, mock_update):
+        deploy = db_deploy.Deployment(deployment=self.deployment)
+        deploy.update_name('new_name')
+        mock_update.assert_called_once_with(
+            self.deployment['uuid'],
+            {'name': 'new_name'},
+        )
+
+    @mock.patch('rally.db.deploy.db.deployment_update')
+    def test_update_config(self, mock_update):
+        deploy = db_deploy.Deployment(deployment=self.deployment)
+        deploy.update_config({'opt': 'val'})
+        mock_update.assert_called_once_with(
+            self.deployment['uuid'],
+            {'config': {'opt': 'val'}},
+        )
+
+    @mock.patch('rally.db.deploy.db.deployment_update')
+    def test_update_endpoint(self, mock_update):
+        deploy = db_deploy.Deployment(deployment=self.deployment)
+        deploy.update_endpoint({'opt': 'val'})
+        mock_update.assert_called_once_with(
+            self.deployment['uuid'],
+            {'endpoint': {'opt': 'val'}},
+        )
