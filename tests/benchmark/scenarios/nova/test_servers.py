9aaa6503ca7bc660bb5242b8c7a3362555ab2384
Boris Pavlovic <boris@pavlovic.me>
2013-11-21 21:52:43 +0400

    Refactor nova scenarios benchmark tests
    Use @mock.patch instanead of with mock.patch() statement
    to avoid horrible nasting
    
    Change-Id: I0140bf0565cc6a9f17a60ed10b7dbe39a2085946

--- tests/benchmark/scenarios/nova/test_servers.py
+++ tests/benchmark/scenarios/nova/test_servers.py
@@ -22,85 +22,73 @@ from rally import test
 from tests.benchmark.scenarios.nova import test_utils
 
 
+NOVA_SERVERS = "rally.benchmark.scenarios.nova.servers.NovaServers"
+
+
 class NovaServersTestCase(test.TestCase):
 
-    def setUp(self):
-        super(NovaServersTestCase, self).setUp()
-        self.scenario = "rally.benchmark.scenarios.nova.servers.NovaServers"
-        self.boot = "%s._boot_server" % self.scenario
-        self.delete = "%s._delete_server" % self.scenario
-        self.random_name = "%s._generate_random_name" % self.scenario
-        self.reboot = "%s._reboot_server" % self.scenario
-        self.start = "%s._start_server" % self.scenario
-        self.stop = "%s._stop_server" % self.scenario
-        self.stop_start = "%s._stop_and_start_server" % self.scenario
-        self.sleep_between = "%s.sleep_between" % self.scenario
-        self.random_choice = "rally.benchmark.scenarios.nova.servers."\
-            "random.choice"
-        self.osclients = "rally.benchmark.utils.osclients"
-
-    def _verify_boot_and_delete_server(self):
+    @mock.patch(NOVA_SERVERS + ".sleep_between")
+    @mock.patch(NOVA_SERVERS + "._generate_random_name")
+    @mock.patch(NOVA_SERVERS + "._delete_server")
+    @mock.patch(NOVA_SERVERS + "._boot_server")
+    def _verify_boot_and_delete_server(self, mock_boot, mock_delete,
+                                       mock_random_name, mock_sleep):
         fake_server = object()
-        with mock.patch(self.boot) as mock_boot:
-            with mock.patch(self.delete) as mock_delete:
-                with mock.patch(self.random_name) as mock_random_name:
-                    with mock.patch(self.sleep_between) as mock_sleep:
-                        mock_boot.return_value = fake_server
-                        mock_random_name.return_value = "random_name"
-                        servers.NovaServers.boot_and_delete_server({}, "img",
-                                                                   0, 10, 20,
-                                                                   fakearg="f")
+        mock_boot.return_value = fake_server
+        mock_random_name.return_value = "random_name"
+        servers.NovaServers.boot_and_delete_server({}, "img", 0, 10, 20,
+                                                   fakearg="f")
 
         mock_boot.assert_called_once_with("random_name", "img", 0, fakearg="f")
         mock_sleep.assert_called_once_with(10, 20)
         mock_delete.assert_called_once_with(fake_server)
 
-    def _verify_boot_server(self, nic=None, assert_nic=False):
+    @mock.patch(NOVA_SERVERS + "._generate_random_name")
+    @mock.patch(NOVA_SERVERS + "._boot_server")
+    @mock.patch("rally.benchmark.utils.osclients")
+    @mock.patch("rally.benchmark.scenarios.nova.servers.random.choice")
+    def _verify_boot_server(self, mock_choice, mock_osclients, mock_boot,
+                            mock_random_name, nic=None, assert_nic=False):
         assert_nic = nic or assert_nic
         kwargs = {'fakearg': 'f'}
         expected_kwargs = {'fakearg': 'f'}
 
-        with mock.patch(self.osclients) as mock_osclients:
-            fc = test_utils.FakeClients()
-            mock_osclients.Clients.return_value = fc
-            nova = test_utils.FakeNovaClient()
-            fc.get_nova_client = lambda: nova
+        fc = test_utils.FakeClients()
+        mock_osclients.Clients.return_value = fc
+        nova = test_utils.FakeNovaClient()
+        fc.get_nova_client = lambda: nova
 
-            temp_keys = ["username", "password", "tenant_name", "uri"]
-            users_endpoints = [dict(zip(temp_keys, temp_keys))]
-            servers.NovaServers.clients = butils._create_openstack_clients(
+        temp_keys = ["username", "password", "tenant_name", "uri"]
+        users_endpoints = [dict(zip(temp_keys, temp_keys))]
+        servers.NovaServers.clients = butils._create_openstack_clients(
                                                 users_endpoints, temp_keys)[0]
 
-            with mock.patch(self.boot) as mock_boot:
-                with mock.patch(self.random_name) as mock_random_name:
-                    with mock.patch(self.random_choice) as mock_choice:
-                        mock_boot.return_value = object()
-                        mock_random_name.return_value = "random_name"
-                        if nic:
-                            kwargs['nics'] = nic
-                        if assert_nic:
-                            nova.networks.create('net-1')
-                            network = nova.networks.create('net-2')
-                            mock_choice.return_value = network
-                            expected_kwargs['nics'] = nic or\
-                                [{'net-id': 'net-2'}]
-                        servers.NovaServers.boot_server({}, "img", 0, **kwargs)
+        mock_boot.return_value = object()
+        mock_random_name.return_value = "random_name"
+        if nic:
+            kwargs['nics'] = nic
+        if assert_nic:
+            nova.networks.create('net-1')
+            network = nova.networks.create('net-2')
+            mock_choice.return_value = network
+            expected_kwargs['nics'] = nic or [{'net-id': 'net-2'}]
+        servers.NovaServers.boot_server({}, "img", 0, **kwargs)
 
         mock_boot.assert_called_once_with("random_name", "img", 0,
                                           **expected_kwargs)
 
-    def test_boot_stop_start(self):
+    @mock.patch(NOVA_SERVERS + "._generate_random_name")
+    @mock.patch(NOVA_SERVERS + "._stop_server")
+    @mock.patch(NOVA_SERVERS + "._start_server")
+    @mock.patch(NOVA_SERVERS + "._boot_server")
+    def test_boot_stop_start(self, mock_boot, mock_start, mock_stop,
+                             mock_name):
         actions = [{'stop_start': 5}]
         fake_server = object()
-        with mock.patch(self.boot) as mock_boot:
-            with mock.patch(self.start) as mock_start:
-                with mock.patch(self.stop) as mock_stop:
-                    with mock.patch(self.random_name) as mock_name:
-                        mock_boot.return_value = fake_server
-                        mock_name.return_value = 'random_name'
-                        servers.NovaServers.boot_and_bounce_server(
-                                                            {}, "img",
-                                                            1, actions=actions)
+        mock_boot.return_value = fake_server
+        mock_name.return_value = 'random_name'
+        servers.NovaServers.boot_and_bounce_server({}, "img", 1,
+                                                   actions=actions)
         mock_boot.assert_called_once_with("random_name", "img", 1,
                                           actions=actions)
         server_calls = []
@@ -119,19 +107,17 @@ class NovaServersTestCase(test.TestCase):
         if mock_stop_start:
             bindings['stop_start']['action'] = mock_stop_start
 
-    def _verify_reboot(self, soft=True):
+    @mock.patch(NOVA_SERVERS + "._generate_random_name")
+    @mock.patch(NOVA_SERVERS + "._reboot_server")
+    @mock.patch(NOVA_SERVERS + "._boot_server")
+    def _verify_reboot(self, mock_boot, mock_reboot, mock_name, soft=True):
         actions = [{'soft_reboot' if soft else 'hard_reboot': 5}]
         fake_server = object()
-        with mock.patch(self.boot) as mock_boot:
-            with mock.patch(self.reboot) as mock_reboot:
-                with mock.patch(self.random_name) as mock_name:
-                    self._bind_server_actions(mock_reboot, None)
-                    mock_boot.return_value = fake_server
-                    mock_name.return_value = 'random_name'
-                    servers.NovaServers.boot_and_bounce_server({},
-                                                               "img",
-                                                               1,
-                                                               actions=actions)
+        self._bind_server_actions(mock_reboot, None)
+        mock_boot.return_value = fake_server
+        mock_name.return_value = 'random_name'
+        servers.NovaServers.boot_and_bounce_server({}, "img", 1,
+                                                   actions=actions)
         mock_boot.assert_called_once_with("random_name", "img", 1,
                                           actions=actions)
         server_calls = []
@@ -141,21 +127,21 @@ class NovaServersTestCase(test.TestCase):
                          "Reboot not called 5 times")
         mock_reboot.assert_has_calls(server_calls)
 
-    def test_multiple_bounce_actions(self):
+    @mock.patch(NOVA_SERVERS + "._generate_random_name")
+    @mock.patch(NOVA_SERVERS + "._stop_and_start_server")
+    @mock.patch(NOVA_SERVERS + "._reboot_server")
+    @mock.patch(NOVA_SERVERS + "._boot_server")
+    def test_multiple_bounce_actions(self, mock_boot, mock_reboot,
+                                     mock_stop_start, mock_name):
         actions = [{'hard_reboot': 5}, {'stop_start': 8}]
         fake_server = object()
-        with mock.patch(self.boot) as mock_boot:
-            with mock.patch(self.reboot) as mock_reboot:
-                with mock.patch(self.random_name) as mock_name:
-                    with mock.patch(self.stop_start) as mock_stop_start:
-                        self._bind_server_actions(mock_reboot, mock_stop_start)
-                        mock_boot.return_value = fake_server
-                        mock_name.return_value = 'random_name'
-                        servers.NovaServers.boot_and_bounce_server(
-                                                            {}, "img", 1,
-                                                            actions=actions)
-        mock_boot.assert_called_once_with("random_name", "img",
-                                          1, actions=actions)
+        self._bind_server_actions(mock_reboot, mock_stop_start)
+        mock_boot.return_value = fake_server
+        mock_name.return_value = 'random_name'
+        servers.NovaServers.boot_and_bounce_server({}, "img", 1,
+                                                   actions=actions)
+        mock_boot.assert_called_once_with("random_name", "img", 1,
+                                          actions=actions)
         server_calls = []
         for i in range(5):
             server_calls.append(mock.call(fake_server, soft=False))
@@ -210,28 +196,23 @@ class NovaServersTestCase(test.TestCase):
     def test_boot_server_random_nic(self):
         self._verify_boot_server(nic=None, assert_nic=True)
 
-    def test_snapshot_server(self):
+    @mock.patch(NOVA_SERVERS + "._generate_random_name")
+    @mock.patch(NOVA_SERVERS + "._delete_image")
+    @mock.patch(NOVA_SERVERS + "._delete_server")
+    @mock.patch(NOVA_SERVERS + "._create_image")
+    @mock.patch(NOVA_SERVERS + "._boot_server")
+    def test_snapshot_server(self, mock_boot, mock_create_image,
+                             mock_delete_server, mock_delete_image,
+                             mock_random_name):
 
         fake_server = object()
         fake_image = test_utils.FakeImageManager().create()
         fake_image.id = "image_id"
 
-        scenario = "rally.benchmark.scenarios.nova.servers.NovaServers"
-        boot = "%s._boot_server" % scenario
-        create_image = "%s._create_image" % scenario
-        delete_server = "%s._delete_server" % scenario
-        delete_image = "%s._delete_image" % scenario
-        random_name = "%s._generate_random_name" % scenario
-        with mock.patch(boot) as mock_boot:
-            with mock.patch(create_image) as mock_create_image:
-                with mock.patch(delete_server) as mock_delete_server:
-                    with mock.patch(delete_image) as mock_delete_image:
-                        with mock.patch(random_name) as mock_random_name:
-                            mock_random_name.return_value = "random_name"
-                            mock_boot.return_value = fake_server
-                            mock_create_image.return_value = fake_image
-                            servers.NovaServers.snapshot_server({}, "i", 0,
-                                                                fakearg=2)
+        mock_random_name.return_value = "random_name"
+        mock_boot.return_value = fake_server
+        mock_create_image.return_value = fake_image
+        servers.NovaServers.snapshot_server({}, "i", 0, fakearg=2)
 
         mock_boot.assert_has_calls([
             mock.call("random_name", "i", 0, fakearg=2),
