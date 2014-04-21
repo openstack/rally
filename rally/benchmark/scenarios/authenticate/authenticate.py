3bbeeefcdc29fb9db6749680f829006eadea0b8d
Neependra Khare <nkhare@redhat.com>
2014-04-21 15:30:44 +0530

    Benchmark to validate a keystone token N times at service endpoint
    With PKI, tokens can be validated at the service endpoint. There might
    be some caching policies which might affect the validation process.
    This benchmark can be used to check effect of caching related to tokens.
    
    Change-Id: I6277d8514bbdb1e5dca5032f44ac3d5ff9618cc2

--- rally/benchmark/scenarios/authenticate/authenticate.py
+++ rally/benchmark/scenarios/authenticate/authenticate.py
@@ -13,6 +13,8 @@
 #    under the License.
 
 from rally.benchmark.scenarios import base
+from rally.benchmark.scenarios import utils as scenario_utils
+from rally.benchmark import validation
 
 
 class Authenticate(base.Scenario):
@@ -21,5 +23,78 @@ class Authenticate(base.Scenario):
     """
 
     @base.scenario()
+    @scenario_utils.atomic_action_timer('authenticate.keystone')
     def keystone(self, **kwargs):
         self.clients("keystone")
+
+    @base.scenario()
+    @validation.add(validation.required_parameters(['repetitions']))
+    def validate_glance(self, repetitions):
+        """Creation of the client does not ensure validation of the token.
+        We have to do some minimal operation to make sure token gets validated.
+        In following we are checking for non-existent image.
+
+        :param repetitions: number of times to validate
+        """
+        glance_client = self.clients("glance")
+        image_name = "__intentionally_non_existent_image___"
+        for i in range(repetitions):
+            with scenario_utils.AtomicAction(self,
+                                             'authenticate.validate_glance'):
+                list(glance_client.images.list(name=image_name))
+
+    @base.scenario()
+    @validation.add(validation.required_parameters(['repetitions']))
+    def validate_nova(self, repetitions):
+        """Creation of the client does not ensure validation of the token.
+        We have to do some minimal operation to make sure token gets validated.
+
+        :param repetitions: number of times to validate
+        """
+        nova_client = self.clients("nova")
+        for i in range(repetitions):
+            with scenario_utils.AtomicAction(self,
+                                             'authenticate.validate_nova'):
+                nova_client.flavors.list()
+
+    @base.scenario()
+    @validation.add(validation.required_parameters(['repetitions']))
+    def validate_cinder(self, repetitions):
+        """Creation of the client does not ensure validation of the token.
+        We have to do some minimal operation to make sure token gets validated.
+
+        :param repetitions: number of times to validate
+        """
+        cinder_client = self.clients("cinder")
+        for i in range(repetitions):
+            with scenario_utils.AtomicAction(self,
+                                             'authenticate.validate_cinder'):
+                cinder_client.volume_types.list()
+
+    @base.scenario()
+    @validation.add(validation.required_parameters(['repetitions']))
+    def validate_neutron(self, repetitions):
+        """Creation of the client does not ensure validation of the token.
+        We have to do some minimal operation to make sure token gets validated.
+
+        :param repetitions: number of times to validate
+        """
+        neutron_client = self.clients("neutron")
+        for i in range(repetitions):
+            with scenario_utils.AtomicAction(self,
+                                             'authenticate.validate_neutron'):
+                neutron_client.get_auth_info()
+
+    @base.scenario()
+    @validation.add(validation.required_parameters(['repetitions']))
+    def validate_heat(self, repetitions):
+        """Creation of the client does not ensure validation of the token.
+        We have to do some minimal operation to make sure token gets validated.
+
+        :param repetitions: number of times to validate
+        """
+        heat_client = self.clients("heat")
+        for i in range(repetitions):
+            with scenario_utils.AtomicAction(self,
+                                             'authenticate.validate_heat'):
+                list(heat_client.stacks.list(limit=0))
