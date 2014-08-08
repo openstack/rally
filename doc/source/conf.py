d06f7c01ca8228cb0534b77e387b87bf222393b7
Swapnil Kulkarni <coolsvap@redhat.com>
2014-08-08 17:55:30 +0000

    Fix copyright in doc/source/config.py
    Mirantis is raplaced by OpenStack Foundation
    
    https://trello.com/c/RdKHZqlq
    
    Change-Id: I371db495da0b14ddaa8e9473dd559880d4990752

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -55,7 +55,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Rally'
-copyright = u'%d, Mirantis Inc.' % datetime.datetime.now().year
+copyright = u'%d, OpenStack Foundation' % datetime.datetime.now().year
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
