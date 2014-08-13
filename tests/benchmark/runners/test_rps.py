612bb23ce2571e0816cca7e77e616d31d1a82447
Oleh Anufriiev <oanufriev@mirantis.com>
2014-08-13 21:15:20 +0300

    Refactoring rps runner unit tests
      Function worker_thread is moved out of worker_process cause difficult to
      test it when it's nested.
    
    Change-Id: I36d4fa91cfc96769087011f56a848e5987bdba24

--- tests/benchmark/runners/test_rps.py
+++ tests/benchmark/runners/test_rps.py
@@ -47,18 +47,77 @@ class RPSScenarioRunnerTestCase(test.TestCase):
         self.assertRaises(jsonschema.ValidationError,
                           rps.RPSScenarioRunner.validate, config)
 
-    def test_run_scenario(self):
+    @mock.patch("rally.benchmark.runners.base.scenario_base")
+    @mock.patch("rally.benchmark.runners.base.osclients")
+    def test_get_rps_runner(self, mock_osclients, mock_base):
+
+        mock_osclients.Clients.return_value = fakes.FakeClients()
+
+        runner = base.ScenarioRunner.get_runner(mock.MagicMock(),
+                                                self.fake_endpoints,
+                                                {"type":
+                                                 consts.RunnerType.RPS})
+        self.assertIsNotNone(runner)
+
+    @mock.patch("rally.benchmark.runners.rps.time.sleep")
+    @mock.patch("rally.benchmark.runners.rps.threading.Thread")
+    @mock.patch("rally.benchmark.runners.rps.multiprocessing.Queue")
+    def test__worker_process(self, mock_queue, mock_thread, mock_sleep):
+        mock_thread_instance = mock.MagicMock(
+            isAlive=mock.MagicMock(return_value=False))
+        mock_thread.return_value = mock_thread_instance
+
+        times = 4
+
+        rps._worker_process(10, times, mock_queue, None, 600, 1,
+                            "Dummy", "dummy", ())
+
+        self.assertEqual(times, mock_thread.call_count)
+
+        self.assertEqual(times, mock_thread_instance.start.call_count)
+        self.assertEqual(times, mock_thread_instance.join.call_count)
+        self.assertEqual(times, mock_thread_instance.isAlive.call_count)
+
+        for i in range(1, times + 1):
+            call = mock.call(args=(mock_queue,
+                                   ("1:%d" % i, "Dummy", "dummy",
+                                    None, ())),
+                             target=rps._worker_thread)
+
+            self.assertIn(call, mock_thread.mock_calls)
+
+    @mock.patch("rally.benchmark.runners.rps.base",
+                _run_scenario_once=mock.MagicMock())
+    def test__worker_thread(self, mock_base):
+        mock_queue = mock.MagicMock()
+
+        args = ("some_args",)
+
+        rps._worker_thread(mock_queue, args)
+
+        mock_queue.put.assert_called_once()
+
+        expected_calls = [mock.call(("some_args",))]
+        self.assertEqual(expected_calls,
+                         mock_base._run_scenario_once.mock_calls)
+
+    @mock.patch("rally.benchmark.runners.rps.time.sleep")
+    def test__run_scenario(self, mock_sleep):
         context = fakes.FakeUserContext({}).context
         context['task'] = {'uuid': 'fake_uuid'}
-        config = {"times": 3, "rps": 10, "timeout": 5}
+        config = {"times": 20, "rps": 20, "timeout": 5}
         runner = rps.RPSScenarioRunner(
                         None, [context["admin"]["endpoint"]], config)
 
         runner._run_scenario(fakes.FakeScenario, "do_it", context, {})
+
+        self.assertEqual(len(runner.result_queue), config["times"])
+
         for result in runner.result_queue:
             self.assertIsNotNone(base.ScenarioRunnerResult(result))
 
-    def test_run_scenario_exception(self):
+    @mock.patch("rally.benchmark.runners.rps.time.sleep")
+    def test__run_scenario_exception(self, mock_sleep):
         context = fakes.FakeUserContext({}).context
         context['task'] = {'uuid': 'fake_uuid'}
 
@@ -71,17 +130,3 @@ class RPSScenarioRunnerTestCase(test.TestCase):
         self.assertEqual(len(runner.result_queue), config["times"])
         for result in runner.result_queue:
             self.assertIsNotNone(base.ScenarioRunnerResult(result))
-
-    @mock.patch("rally.benchmark.runners.base.scenario_base")
-    @mock.patch("rally.benchmark.runners.base.osclients")
-    def test_get_rps_runner(self, mock_osclients, mock_base):
-        FakeScenario = mock.MagicMock()
-        FakeScenario.init = mock.MagicMock(return_value={})
-
-        mock_osclients.Clients.return_value = fakes.FakeClients()
-
-        runner = base.ScenarioRunner.get_runner(mock.MagicMock(),
-                                                self.fake_endpoints,
-                                                {"type":
-                                                 consts.RunnerType.RPS})
-        self.assertIsNotNone(runner)
