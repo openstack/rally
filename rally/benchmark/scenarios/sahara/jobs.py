90064d01397694d1b3070691b386ce83da6caef4
Nikita Konovalov <nkonovalov@mirantis.com>
2014-08-26 16:32:43 +0400

    Sahara Job Execution benchmark
    The benchmark for job execution added.
    
    Change-Id: Iac251de92413f0f1fe1c872b5183c91e40eb1232

---
+++ rally/benchmark/scenarios/sahara/jobs.py
@@ -0,0 +1,66 @@
+# Copyright 2014: Mirantis Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from rally.benchmark.scenarios import base
+from rally.benchmark.scenarios.sahara import utils
+from rally.benchmark import validation
+from rally import consts
+from rally.openstack.common import log as logging
+
+LOG = logging.getLogger(__name__)
+
+
+class SaharaJob(utils.SaharaScenario):
+
+    @validation.required_services(consts.Service.SAHARA)
+    @validation.required_contexts("users", "sahara_image", "sahara_edp",
+                                  "sahara_cluster")
+    @base.scenario(context={"cleanup": ["sahara"]})
+    def create_launch_job(self, job_type, configs):
+        """Test the Sahara EDP Job execution.
+
+        :param job_type: The type of the Data Processing Job
+        :param configs: The configs dict that will be passed to a Job Execution
+
+        This scenario Creates a Job entity and launches an execution on a
+        Cluster.
+
+        """
+
+        tenant_id = self.clients("keystone").tenant_id
+        mains = self.context()["sahara_mains"][tenant_id]
+        libs = self.context()["sahara_libs"][tenant_id]
+
+        name = self._generate_random_name(prefix="job_")
+        job = self.clients("sahara").jobs.create(name=name,
+                                                 type=job_type,
+                                                 description="",
+                                                 mains=mains,
+                                                 libs=libs)
+
+        cluster_id = self.context()["sahara_clusters"][tenant_id]
+
+        if job_type.lower() == "java":
+            input_id = None
+            output_id = None
+        else:
+            input_id = self.context()["sahara_inputs"][tenant_id]
+            output_id = self._create_output_ds().id
+
+        self._run_job_execution(job_id=job.id,
+                                cluster_id=cluster_id,
+                                input_id=input_id,
+                                output_id=output_id,
+                                configs=configs)
