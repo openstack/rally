4b448c0f278032f55ada53242ee9221ea20683e3
Pavel Boldin <pboldin@mirantis.com>
2014-11-11 20:33:12 +0200

    Removing redundancy in (a==b) and 1 or 0
    Change-Id: I95ac07b1199dc2282b7a238e6fe44477885226c3

--- rally/utils.py
+++ rally/utils.py
@@ -328,6 +328,6 @@ def distance(s1, s2):
     for y in range(1, len(s2) + 1):
         l, n = n, [y]
         for x in xrange(1, len(s1) + 1):
-            n.append(min(l[x] + 1, n[-1] + 1, l[x - 1] +
-                     ((s2[y - 1] != s1[x - 1]) and 1 or 0)))
+            n.append(min(l[x] + 1, n[-1] + 1,
+                         l[x - 1] + (s2[y - 1] != s1[x - 1])))
     return n[-1]
