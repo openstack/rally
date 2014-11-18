e12ccc7d0962f2789f7c2b57e5fa6757d39628ea
sskripnick <sskripnick@sskripnick-pc.kha.mirantis.net>
2014-11-18 14:43:14 +0200

    Fix html rendering on failed functional tests
    Remove -e option from shebang
    
    Change-Id: I32fb1c13e20a61476958eabfca7897caa59dccab

--- tests/ci/rally-integrated.sh
+++ tests/ci/rally-integrated.sh
@@ -1,12 +1,12 @@
-#!/bin/sh -ex
+#!/bin/bash -x
 
 env
 
 mkdir -p .testrepository
 python -m subunit.run discover tests/functional > .testrepository/subunit.log
-EXIT_CODE=$?
 
 subunit2pyunit < .testrepository/subunit.log
+EXIT_CODE=$?
 subunit-stats < .testrepository/subunit.log
 
 exit $EXIT_CODE
