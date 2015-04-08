8ab770fdd80af265619ddbe6d4f244c87a1c50c3
Chris St. Pierre <chris.a.st.pierre@gmail.com>
2015-04-08 11:10:25 -0500

    Keystone: Add scenarios to benchmark user disable/enable
    Adds a new scenario to create a user and toggle the enabled flag.
    
    Change-Id: I0b8d8e57c95cced72b6615f8120ec41879c414be

--- tests/unit/plugins/openstack/scenarios/keystone/test_utils.py
+++ tests/unit/plugins/openstack/scenarios/keystone/test_utils.py
@@ -64,6 +64,19 @@ class KeystoneScenarioTestCase(test.ClientsTestCase):
         self._test_atomic_action_timer(scenario.atomic_actions(),
                                        "keystone.create_user")
 
+    def test_update_user_enabled(self):
+        user = mock.Mock()
+        enabled = mock.Mock()
+        scenario = utils.KeystoneScenario()
+
+        scenario._update_user_enabled(user, enabled)
+        self.admin_clients(
+            "keystone").users.update_enabled.assert_called_once_with(user,
+                                                                     enabled)
+
+        self._test_atomic_action_timer(scenario.atomic_actions(),
+                                       "keystone.update_user_enabled")
+
     @mock.patch("rally.common.utils.generate_random_name")
     def test_role_create(self, mock_generate_random_name):
         scenario = utils.KeystoneScenario()
