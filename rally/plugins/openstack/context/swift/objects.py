b6dc5a9763aaae1304736054719769841ed2d098
John Wu <weiwu@cisco.com>
2015-05-02 05:55:41 -0700

    [Swift] Add objects context class
    This patch adds new context for creating swift objects using broker
    pattern.  It will create large number of containers and objects
    concurrently before benchmark task like download or list objects and
    delete it after task finishes.
    
    Blueprint benchmark-scenarios-for-swift
    
    Change-Id: I0c8623fbe56401171244542ad8188d20f8848e8c

---
+++ rally/plugins/openstack/context/swift/objects.py
@@ -0,0 +1,100 @@
+# Copyright 2015: Cisco Systems, Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from rally.common.i18n import _
+from rally.common import log as logging
+from rally.common import utils as rutils
+from rally import consts
+from rally import exceptions
+from rally.plugins.openstack.context.swift import utils as swift_utils
+from rally.task import context
+
+LOG = logging.getLogger(__name__)
+
+
+@context.configure(name="swift_objects", order=360)
+class SwiftObjectGenerator(swift_utils.SwiftObjectMixin, context.Context):
+    CONFIG_SCHEMA = {
+        "type": "object",
+        "$schema": consts.JSON_SCHEMA,
+        "properties": {
+            "containers_per_tenant": {
+                "type": "integer",
+                "minimum": 1
+            },
+            "objects_per_container": {
+                "type": "integer",
+                "minimum": 1
+            },
+            "object_size": {
+                "type": "integer",
+                "minimum": 1
+            },
+            "resource_management_workers": {
+                "type": "integer",
+                "minimum": 1
+            }
+        },
+        "additionalProperties": False
+    }
+
+    DEFAULT_CONFIG = {
+        "containers_per_tenant": 1,
+        "objects_per_container": 1,
+        "object_size": 1024,
+        "resource_management_workers": 30
+    }
+
+    @rutils.log_task_wrapper(LOG.info, _("Enter context: `swift_objects`"))
+    def setup(self):
+        """Create containers and objects, using the broker pattern."""
+        threads = self.config["resource_management_workers"]
+
+        containers_per_tenant = self.config["containers_per_tenant"]
+        containers_num = len(self.context["tenants"]) * containers_per_tenant
+        LOG.debug("Creating %d containers using %d threads." % (containers_num,
+                                                                threads))
+        containers_count = len(self._create_containers(self.context,
+                                                       containers_per_tenant,
+                                                       threads))
+        if containers_count != containers_num:
+            raise exceptions.ContextSetupFailure(
+                ctx_name=self.get_name(),
+                msg=_("Failed to create the requested number of containers, "
+                      "expected %(expected)s but got %(actual)s.")
+                % {"expected": containers_num, "actual": containers_count})
+
+        objects_per_container = self.config["objects_per_container"]
+        objects_num = containers_num * objects_per_container
+        LOG.debug("Creating %d objects using %d threads." % (objects_num,
+                                                             threads))
+        objects_count = len(self._create_objects(self.context,
+                                                 objects_per_container,
+                                                 self.config["object_size"],
+                                                 threads))
+        if objects_count != objects_num:
+            raise exceptions.ContextSetupFailure(
+                ctx_name=self.get_name(),
+                msg=_("Failed to create the requested number of objects, "
+                      "expected %(expected)s but got %(actual)s.")
+                % {"expected": objects_num, "actual": objects_count})
+
+    @rutils.log_task_wrapper(LOG.info, _("Exit context: `swift_objects`"))
+    def cleanup(self):
+        """Delete containers and objects, using the broker pattern."""
+        threads = self.config["resource_management_workers"]
+
+        self._delete_objects(self.context, threads)
+        self._delete_containers(self.context, threads)
---
+++ tests/unit/plugins/openstack/context/swift/test_objects.py
@@ -0,0 +1,204 @@
+# Copyright 2015: Cisco Systems, Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import mock
+
+from rally import exceptions
+from rally.plugins.openstack.context.swift import objects
+from tests.unit import test
+
+
+class SwiftObjectGeneratorTestCase(test.TestCase):
+
+    @mock.patch("rally.osclients.Clients")
+    def test_setup(self, mock_clients):
+        containers_per_tenant = 2
+        objects_per_container = 7
+        context = {
+            "config": {
+                "swift_objects": {
+                    "containers_per_tenant": containers_per_tenant,
+                    "objects_per_container": objects_per_container,
+                    "object_size": 1024,
+                    "resource_management_workers": 10
+                }
+            },
+            "task": {"uuid": "id123"},
+            "tenants": {
+                "t1": {"name": "t1_name"},
+                "t2": {"name": "t2_name"}
+            },
+            "users": [
+                {"id": "u1", "tenant_id": "t1", "endpoint": "e1"},
+                {"id": "u2", "tenant_id": "t2", "endpoint": "e2"}
+            ]
+        }
+
+        objects_ctx = objects.SwiftObjectGenerator(context)
+        objects_ctx.setup()
+
+        for tenant_id in context["tenants"]:
+            containers = context["tenants"][tenant_id]["containers"]
+            self.assertEqual(containers_per_tenant, len(containers))
+            for container in containers:
+                self.assertIn("rally_container_", container["container"])
+                self.assertEqual(objects_per_container,
+                                 len(container["objects"]))
+                for obj in container["objects"]:
+                    self.assertIn("rally_object_", obj)
+
+    @mock.patch("rally.osclients.Clients")
+    @mock.patch("rally.plugins.openstack.context.swift.utils."
+                "swift_utils.SwiftScenario")
+    def test_cleanup(self, mock_swift_scenario, mock_clients):
+        context = {
+            "config": {
+                "swift_objects": {
+                    "resource_management_workers": 1
+                }
+            },
+            "task": {"uuid": "id123"},
+            "tenants": {
+                "t1": {
+                    "name": "t1_name",
+                    "containers": [
+                        {"user": {"id": "u1", "tenant_id": "t1",
+                                  "endpoint": "e1"},
+                         "container": "c1",
+                         "objects": ["o1", "o2", "o3"]}
+                    ]
+                },
+                "t2": {
+                    "name": "t2_name",
+                    "containers": [
+                        {"user": {"id": "u2", "tenant_id": "t2",
+                                  "endpoint": "e2"},
+                         "container": "c2",
+                         "objects": ["o4", "o5", "o6"]}
+                    ]
+                }
+            }
+        }
+
+        objects_ctx = objects.SwiftObjectGenerator(context)
+        objects_ctx.cleanup()
+
+        expected_containers = ["c1", "c2"]
+        mock_swift_scenario.return_value._delete_container.assert_has_calls(
+            [mock.call(con) for con in expected_containers], any_order=True)
+
+        expected_objects = [("c1", "o1"), ("c1", "o2"), ("c1", "o3"),
+                            ("c2", "o4"), ("c2", "o5"), ("c2", "o6")]
+        mock_swift_scenario.return_value._delete_object.assert_has_calls(
+            [mock.call(con, obj) for con, obj in expected_objects],
+            any_order=True)
+
+        for tenant_id in context["tenants"]:
+            self.assertEqual(0,
+                             len(context["tenants"][tenant_id]["containers"]))
+
+    @mock.patch("rally.osclients.Clients")
+    def test_setup_failure_clients_put_container(self, mock_clients):
+        context = {
+            "config": {
+                "swift_objects": {
+                    "containers_per_tenant": 2,
+                    "object_size": 10,
+                    "resource_management_workers": 5
+                }
+            },
+            "task": {"uuid": "id123"},
+            "tenants": {
+                "t1": {"name": "t1_name"},
+                "t2": {"name": "t2_name"}
+            },
+            "users": [
+                {"id": "u1", "tenant_id": "t1", "endpoint": "e1"},
+                {"id": "u2", "tenant_id": "t2", "endpoint": "e2"}
+            ]
+        }
+        mock_swift = mock_clients.return_value.swift.return_value
+        mock_swift.put_container.side_effect = [Exception, True,
+                                                Exception, Exception]
+        objects_ctx = objects.SwiftObjectGenerator(context)
+        self.assertRaisesRegexp(exceptions.ContextSetupFailure,
+                                "containers, expected 4 but got 1",
+                                objects_ctx.setup)
+
+    @mock.patch("rally.osclients.Clients")
+    def test_setup_failure_clients_put_object(self, mock_clients):
+        context = {
+            "task": {"uuid": "id123"},
+            "tenants": {
+                "t1": {"name": "t1_name"},
+                "t2": {"name": "t2_name"}
+            },
+            "users": [
+                {"id": "u1", "tenant_id": "t1", "endpoint": "e1"},
+                {"id": "u2", "tenant_id": "t2", "endpoint": "e2"}
+            ]
+        }
+        mock_swift = mock_clients.return_value.swift.return_value
+        mock_swift.put_object.side_effect = [Exception, True]
+        objects_ctx = objects.SwiftObjectGenerator(context)
+        self.assertRaisesRegexp(exceptions.ContextSetupFailure,
+                                "objects, expected 2 but got 1",
+                                objects_ctx.setup)
+
+    @mock.patch("rally.osclients.Clients")
+    def test_cleanup_failure_clients_delete_container(self, mock_clients):
+        context = {
+            "task": {"uuid": "id123"},
+            "tenants": {
+                "t1": {
+                    "name": "t1_name",
+                    "containers": [
+                        {"user": {"id": "u1", "tenant_id": "t1",
+                                  "endpoint": "e1"},
+                         "container": "coooon",
+                         "objects": []}] * 3
+                }
+            }
+        }
+        mock_swift = mock_clients.return_value.swift.return_value
+        mock_swift.delete_container.side_effect = [True, True, Exception]
+        objects_ctx = objects.SwiftObjectGenerator(context)
+        objects_ctx.cleanup()
+        self.assertEqual(1, len(context["tenants"]["t1"]["containers"]))
+
+    @mock.patch("rally.osclients.Clients")
+    def test_cleanup_failure_clients_delete_object(self, mock_clients):
+        context = {
+            "task": {"uuid": "id123"},
+            "tenants": {
+                "t1": {
+                    "name": "t1_name",
+                    "containers": [
+                        {"user": {"id": "u1", "tenant_id": "t1",
+                                  "endpoint": "e1"},
+                         "container": "c1",
+                         "objects": ["oooo"] * 3}
+                    ]
+                }
+            }
+        }
+        mock_swift = mock_clients.return_value.swift.return_value
+        mock_swift.delete_object.side_effect = [True, Exception, True]
+        objects_ctx = objects.SwiftObjectGenerator(context)
+        objects_ctx._delete_containers = mock.MagicMock()
+        objects_ctx.cleanup()
+        self.assertEqual(
+            1, sum([len(container["objects"])
+                    for container in context["tenants"]["t1"]["containers"]]))
