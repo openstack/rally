5fd2b2cc64810ade538fef47b8e7107343945f55
Roman Vasilets <rvasilets@mirantis.com>
2015-05-27 19:13:58 +0300

    Add Ironic scenarios
    Add Ironic scenarios:
      * create_and_list_node
      * create_and_delete_node
    Add Ironic related utils:
      * _create_node
      * _list_nodes
      * _delete_node
    
    Add unit tests for them.
    Add Ironic to _Service and BARE_METAL to _ServiceType.
    Also there is a new rally-ironic.yaml job file.
    
    Change-Id: I5bfa68d2f9dc30680996d932c5b03b01d6c0a0fa

---
+++ tests/unit/plugins/openstack/scenarios/ironic/test_nodes.py
@@ -0,0 +1,56 @@
+# Copyright 2015: Mirantis Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import mock
+
+from rally.plugins.openstack.scenarios.ironic import nodes
+from tests.unit import test
+
+
+class IronicNodesTestCase(test.ScenarioTestCase):
+
+    def test_create_and_list_node(self):
+        scenario = nodes.IronicNodes()
+        scenario._create_node = mock.Mock()
+        scenario._list_nodes = mock.Mock()
+        fake_params = {
+            "sort_dir": "foo1",
+            "associated": "foo2",
+            "sort_key": "foo3",
+            "detail": True,
+            "limit": "foo4",
+            "maintenance": "foo5",
+            "marker": "foo6",
+            "fake_parameter1": "foo7"
+        }
+        scenario.create_and_list_node(**fake_params)
+
+        scenario._create_node.assert_called_once_with(fake_parameter1="foo7")
+        scenario._list_nodes.assert_called_once_with(
+            sort_dir="foo1", associated="foo2", sort_key="foo3", detail=True,
+            limit="foo4", maintenance="foo5", marker="foo6")
+
+    def test_create_and_delete_node(self):
+        fake_node = mock.Mock(uuid="fake_uuid")
+        scenario = nodes.IronicNodes()
+        scenario._create_node = mock.Mock(return_value=fake_node)
+        scenario._delete_node = mock.Mock()
+
+        scenario.create_and_delete_node(fake_parameter1="fake1",
+                                        fake_parameter2="fake2")
+        scenario._create_node.assert_called_once_with(fake_parameter1="fake1",
+                                                      fake_parameter2="fake2")
+
+        scenario._delete_node.assert_called_once_with("fake_uuid")
---
+++ tests/unit/plugins/openstack/scenarios/ironic/test_utils.py
@@ -0,0 +1,68 @@
+# Copyright 2015: Mirantis Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import mock
+
+from rally.plugins.openstack.scenarios.ironic import utils
+from tests.unit import test
+
+IRONIC_UTILS = "rally.plugins.openstack.scenarios.ironic.utils"
+
+
+class IronicScenarioTestCase(test.ScenarioTestCase):
+
+    @mock.patch("rally.common.utils.generate_random_name")
+    def test__create_node(self, mock_generate_random_name):
+        mock_generate_random_name.return_value = "rally_fake_random_string"
+        self.admin_clients("ironic").node.create.return_value = "fake_node"
+        scenario = utils.IronicScenario()
+        create_node = scenario._create_node(fake_param="foo")
+
+        self.assertEqual("fake_node", create_node)
+        self.admin_clients("ironic").node.create.assert_called_once_with(
+            fake_param="foo", name="rally_fake_random_string")
+        self._test_atomic_action_timer(scenario.atomic_actions(),
+                                       "ironic.create_node")
+
+    def test__delete_node(self):
+        mock_node_delete = mock.Mock()
+        self.admin_clients("ironic").node.delete = mock_node_delete
+        scenario = utils.IronicScenario()
+        scenario._delete_node("fake_id")
+
+        self.admin_clients("ironic").node.delete.assert_called_once_with(
+            "fake_id")
+        self._test_atomic_action_timer(scenario.atomic_actions(),
+                                       "ironic.delete_node")
+
+    def test__list_nodes(self):
+        self.admin_clients("ironic").node.list.return_value = ["fake"]
+        scenario = utils.IronicScenario()
+        fake_params = {
+            "sort_dir": "foo1",
+            "associated": "foo2",
+            "sort_key": "foo3",
+            "detail": True,
+            "limit": "foo4",
+            "maintenance": "foo5",
+            "marker": "foo6"
+        }
+        return_nodes_list = scenario._list_nodes(**fake_params)
+        self.assertEqual(["fake"], return_nodes_list)
+        self.admin_clients("ironic").node.list.assert_called_once_with(
+            sort_dir="foo1", associated="foo2", sort_key="foo3", detail=True,
+            limit="foo4", maintenance="foo5", marker="foo6")
+        self._test_atomic_action_timer(scenario.atomic_actions(),
+                                       "ironic.list_nodes")
