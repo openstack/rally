e02f27060bee247bbc706b09189c1fb840ba5dea
Igor Degtiarov <idegtiarov@mirantis.com>
2015-06-09 15:14:39 +0300

    [Ceilometer] Add  scenario for events and traits
    Add scenarios that cover all events and traits Ceilometer API
    
    All scenarios create new user in keystone to store new Event in Ceilometer.
    create_user_and_get_event checks GET /v2/events/<message_id>
    create_user_and_list_events checks GET /v2/events
    create_user_and_list_event_types checks GET /v2/event_types
    create_user_and_list_traits checks
    GET /v2/event_types/<event_type>/traits/<trait_name>
    create_user_and_list_trait_description checks
    GET /v2/event_types/<event_type>/traits
    
    Change-Id: I572730d9f69e3d63b29eeedd03ddc24071ffca3d

---
+++ tests/unit/plugins/openstack/scenarios/ceilometer/test_events.py
@@ -0,0 +1,51 @@
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import mock
+
+from rally.plugins.openstack.scenarios.ceilometer import events
+from tests.unit import test
+
+
+class CeilometerEventsTestCase(test.TestCase):
+
+    def test_list_events(self):
+        scenario = events.CeilometerEvents()
+
+        scenario._user_create = mock.MagicMock()
+        scenario._list_events = mock.MagicMock()
+        scenario.create_user_and_list_events()
+        scenario._user_create.assert_called_once_with()
+        scenario._list_events.assert_called_once_with()
+
+    def test_list_event_types(self):
+        scenario = events.CeilometerEvents()
+
+        scenario._list_event_types = mock.MagicMock()
+        scenario._user_create = mock.MagicMock()
+        scenario.create_user_and_list_event_types()
+        scenario._user_create.assert_called_once_with()
+        scenario._list_event_types.assert_called_once_with()
+
+    def test_get_event(self):
+        scenario = events.CeilometerEvents()
+
+        scenario._user_create = mock.MagicMock()
+        scenario._list_events = mock.MagicMock()
+        scenario._get_event = mock.MagicMock()
+        scenario._list_events.return_value = [mock.Mock(message_id="fake_id")]
+        scenario.create_user_and_get_event()
+        scenario._user_create.assert_called_once_with()
+        scenario._list_events.assert_called_with()
+        scenario._get_event.assert_called_with(event_id="fake_id")
---
+++ tests/unit/plugins/openstack/scenarios/ceilometer/test_traits.py
@@ -0,0 +1,52 @@
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import mock
+
+from rally.plugins.openstack.scenarios.ceilometer import traits
+from tests.unit import test
+
+
+class CeilometerTraitsTestCase(test.TestCase):
+
+    def test_list_traits(self):
+        scenario = traits.CeilometerTraits()
+
+        scenario._user_create = mock.MagicMock()
+        scenario._list_events = mock.MagicMock()
+        scenario._list_event_traits = mock.MagicMock()
+        scenario._list_events.return_value = [mock.Mock(
+            event_type="fake_event_type",
+            traits=[{"name": "fake_trait_name"}])
+        ]
+        scenario.create_user_and_list_traits()
+        scenario._user_create.assert_called_once_with()
+        scenario._list_events.assert_called_with()
+        scenario._list_event_traits.assert_called_once_with(
+            event_type="fake_event_type", trait_name="fake_trait_name")
+
+    def test_list_trait_descriptions(self):
+        scenario = traits.CeilometerTraits()
+
+        scenario._user_create = mock.MagicMock()
+        scenario._list_events = mock.MagicMock()
+        scenario._list_event_trait_descriptions = mock.MagicMock()
+        scenario._list_events.return_value = [mock.Mock(
+            event_type="fake_event_type")
+        ]
+        scenario.create_user_and_list_trait_descriptions()
+        scenario._user_create.assert_called_once_with()
+        scenario._list_events.assert_called_with()
+        scenario._list_event_trait_descriptions.assert_called_once_with(
+            event_type="fake_event_type")
