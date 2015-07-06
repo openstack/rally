c2234812a8e912e37816cf28ff995ce298dce317
Alexander Maretskiy <amaretskiy@mirantis.com>
2015-07-06 18:58:13 +0300

    [Reports] Add class for merging data stream
    This patch adds class rally.task.processing.utils.GraphZiper
    that can process unlimited stream of some values (assume
    that is a kind of iteration value) and compose a result list
    with expected length.
    
    This is useful for processing data for reports charts, so we
    can process arbitrary count of iterations without memory load
    and generate effective chart for these iterations.
    
    Change-Id: Iec28b26fcbeb8523bb0b01cf0723be37c76f0127

--- rally/task/processing/utils.py
+++ rally/task/processing/utils.py
@@ -144,3 +144,66 @@ def compress(data, limit=1000, merge=None, normalize=None):
         result.append((len(data), cur_value))
 
     return result
+
+
+class GraphZipper(object):
+
+    def __init__(self, base_size, zipped_size=1000):
+        """Init graph ziper.
+
+        :param base_size: Amount of points in raw graph
+        :param zip_size: Amount of points that should be in zipped graph
+        """
+        self.base_size = base_size
+        self.zipped_size = zipped_size
+        if self.base_size >= self.zipped_size:
+            self.compression_ratio = self.base_size / float(self.zipped_size)
+        else:
+            self.compression_ratio = 1
+
+        self.point_order = 0
+
+        self.cached_ratios_sum = 0
+        self.ratio_value_points = []
+
+        self.zipped_graph = []
+
+    def _get_zipped_point(self):
+        if self.point_order - self.compression_ratio <= 1:
+            order = 1
+        elif self.point_order == self.base_size:
+            order = self.base_size
+        else:
+            order = self.point_order - int(self.compression_ratio / 2.0)
+
+        value = (
+            sum(p[0] * p[1] for p in self.ratio_value_points) /
+            self.compression_ratio
+        )
+
+        return [order, value]
+
+    def add_point(self, value):
+        self.point_order += 1
+
+        if self.point_order > self.base_size:
+            raise RuntimeError("GraphZipper is already full. "
+                               "You can't add more points.")
+
+        if not isinstance(value, (int, float)):
+            value = 0
+
+        if self.compression_ratio <= 1:    # We don't need to compress
+            self.zipped_graph.append([self.point_order, value])
+        elif self.cached_ratios_sum + 1 < self.compression_ratio:
+            self.cached_ratios_sum += 1
+            self.ratio_value_points.append([1, value])
+        else:
+            rest = self.compression_ratio - self.cached_ratios_sum
+            self.ratio_value_points.append([rest, value])
+            self.zipped_graph.append(self._get_zipped_point())
+            self.ratio_value_points = [[1 - rest, value]]
+            self.cached_ratios_sum = self.ratio_value_points[0][0]
+
+    def get_zipped_graph(self):
+        return self.zipped_graph
--- tests/unit/task/processing/test_utils.py
+++ tests/unit/task/processing/test_utils.py
@@ -13,6 +13,8 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import ddt
+
 from rally import exceptions
 from rally.task.processing import utils
 from tests.unit import test
@@ -161,3 +163,33 @@ class AtomicActionsDataTestCase(test.TestCase):
 
         output = utils.get_atomic_actions_data(raw_data)
         self.assertEqual(output, atomic_actions_data)
+
+
+@ddt.ddt
+class GraphZipperTestCase(test.TestCase):
+
+    @ddt.data({"data_stream": list(range(1, 11)), "zipped_size": 8,
+               "expected": [[1, 1.2], [3, 2.4], [4, 3.6], [5, 4.8], [7, 6.2],
+                            [8, 7.4], [9, 8.6], [10, 9.8]]},
+              {"data_stream": [.005, .8, 22, .004, .7, 12, .5, .07, .02] * 10,
+               "zipped_size": 8, "expected": [
+                   [1, 3.769244444444445], [18, 4.706933333333334],
+                   [29, 4.339911111111111], [40, 3.2279111111111116],
+                   [52, 3.769244444444445], [63, 4.706933333333334],
+                   [74, 4.339911111111111], [90, 3.2279111111111116]]},
+              {"data_stream": list(range(1, 100)), "zipped_size": 1000,
+               "expected": [[i, i] for i in range(1, 100)]},
+              {"data_stream": [1, 4, 11, None, 42], "zipped_size": 1000,
+               "expected": [[1, 1], [2, 4], [3, 11], [4, 0], [5, 42]]})
+    @ddt.unpack
+    def test_add_point_and_get_zipped_graph(self, data_stream=None,
+                                            zipped_size=None, expected=None):
+        merger = utils.GraphZipper(len(data_stream), zipped_size)
+        [merger.add_point(value) for value in data_stream]
+        self.assertEqual(expected, merger.get_zipped_graph())
+
+    def test_add_point_raises(self):
+        merger = utils.GraphZipper(10, 8)
+        self.assertRaises(TypeError, merger.add_point)
+        [merger.add_point(1) for value in range(10)]
+        self.assertRaises(RuntimeError, merger.add_point, 1)
