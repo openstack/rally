ce560cee7392409f7b00ca4dfb322a96263af531
Yaroslav Isakov <yisakov@mirantis.com>
2015-09-18 17:23:46 +0300

    Certification task: macros and cinder checks
    Change-Id: I4b947dc8494ed6184333ac2e473c35ecb3314d31

---
+++ tests/functional/test_certification_task.py
@@ -0,0 +1,36 @@
+# Copyright 2014: Mirantis Inc.
+# Copyright 2014: Catalyst IT Ltd.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import os
+import traceback
+import unittest
+
+from tests.functional import utils
+
+
+class TestCertificationTask(unittest.TestCase):
+
+    def test_task_samples_is_valid(self):
+        rally = utils.Rally()
+        full_path = os.path.join(
+            os.path.dirname(__file__), os.pardir, os.pardir,
+            "certification", "openstack", "task.yaml")
+
+        try:
+            rally("task validate --task %s" % full_path)
+        except Exception:
+            print(traceback.format_exc())
+            self.assertTrue(False, "Wrong task config %s" % full_path)
