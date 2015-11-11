502b38a8311ac188772c203b8887f3c8328b9f21
Igor Degtiarov <idegtiarov@mirantis.com>
2015-11-11 10:44:58 +0200

    Extend statistic benchmarks for Ceilometer
    Existing benchmark scenario for statistics was not benchmark at all
    it was check that statistic api works, now scenario was changed
    with using new Ceilometer context and adding new configured
    query parameters.
    
    It allows to find a weak places in requesting Ceilometer data.
    
    Change-Id: Iccde7422016e87f20237721b7fc1fb379646adf8

--- rally/plugins/openstack/scenarios/ceilometer/stats.py
+++ rally/plugins/openstack/scenarios/ceilometer/stats.py
@@ -12,6 +12,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from rally.common import log
 from rally import consts
 from rally.plugins.openstack import scenario
 from rally.plugins.openstack.scenarios.ceilometer import utils
@@ -21,9 +22,11 @@ from rally.task import validation
 class CeilometerStats(utils.CeilometerScenario):
     """Benchmark scenarios for Ceilometer Stats API."""
 
+    @log.log_deprecated("Use 'get_stats' method, now samples are created in "
+                        "context", "0.1.2")
     @validation.required_services(consts.Service.CEILOMETER)
     @validation.required_openstack(users=True)
-    @scenario.configure(context={"cleanup": ["ceilometer"]})
+    @scenario.configure()
     def create_meter_and_get_stats(self, **kwargs):
         """Create a meter and fetch its statistics.
 
@@ -34,3 +37,32 @@ class CeilometerStats(utils.CeilometerScenario):
         """
         meter = self._create_meter(**kwargs)
         self._get_stats(meter.counter_name)
+
+    @validation.required_services(consts.Service.CEILOMETER)
+    @validation.required_openstack(users=True)
+    @scenario.configure()
+    def get_stats(self, meter_name, filter_by_user_id=False,
+                  filter_by_project_id=False, filter_by_resource_id=False,
+                  metadata_query=None, period=None, groupby=None,
+                  aggregates=None):
+        """Fetch statistics for certain meter.
+
+        Statistics is fetched for the using
+        GET /v2/meters/(meter_name)/statistics.
+
+        :param meter_name: meter to take statistic for
+        :param filter_by_user_id: flag for query by user_id
+        :param filter_by_project_id: flag for query by project_id
+        :param filter_by_resource_id: flag for query by resource_id
+        :param metadata_query: dict with metadata fields and values for query
+        :param period: the length of the time range covered by these stats
+        :param groupby: the fields used to group the samples
+        :param aggregates: name of function for samples aggregation
+
+        :returns: list of statistics data
+        """
+        query = self._make_general_query(filter_by_project_id,
+                                         filter_by_user_id,
+                                         filter_by_resource_id,
+                                         metadata_query)
+        self._get_stats(meter_name, query, period, groupby, aggregates)
