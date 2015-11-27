ec4b0412c5ca272f49f69b1a8014f8c680ab8d45
Andrey Kurilin <akurilin@mirantis.com>
2015-11-27 15:52:19 +0200

    [verify] Remove redundant script
    Required change to infra was merged(https://review.openstack.org/#/c/240580 ),
    so this script is unneeded now.
    
    Change-Id: I292c84f56f0f500bbb095144844c9ce1548c268e

--- tests/ci/rally-verify.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# FIXME(andrekurilin): remove this file, when patch with proper fix in infra
-# (https://review.openstack.org/#/c/240580/) will be merged
-exec $(dirname $0)/rally_verify.py --compare
