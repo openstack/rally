openstack/rally

commit b331e10bfcb5afeccd2108c6b68bf236c8bf6f76

Ensure cleanup context uses the supplied api_versions in the task
When an api_versions supplied in the task the cleanup context
need to use it to make sure proper version is used while cleaning
up the created resources. This fix makes sure the version is
propagated to the cleanup managers

Change-Id: I2cc05a95ad2c69b97f6a7e6bd9410227f8176394

rally/plugins/openstack/cleanup/manager.py
@@ -29,7 +29,7 @@ LOG = logging.getLogger(__name__)
 
 class SeekAndDestroy(object):
 
-    def __init__(self, manager_cls, admin, users):
+    def __init__(self, manager_cls, admin, users, api_versions=None):
         """Resource deletion class.
 
         This class contains method exterminate() that finds and deletes
@@ -42,20 +42,24 @@ class SeekAndDestroy(object):
         self.manager_cls = manager_cls
         self.admin = admin
         self.users = users or []
+        self.api_versions = api_versions
 
     @staticmethod
-    def _get_cached_client(user, cache=None):
+    def _get_cached_client(user, cache=None, api_versions=None):
         """Simplifies initialization and caching OpenStack clients."""
 
         if not user:
             return None
 
         if not isinstance(cache, dict):
-            return osclients.Clients(user["credential"])
+            return osclients.Clients(user["credential"], api_info=api_versions)
 
-        key = user["credential"]
+        if api_versions:
+            key = str((user["credential"], sorted(api_versions.items())))
+        else:
+            key = user["credential"]
         if key not in cache:
-            cache[key] = osclients.Clients(key)
+            cache[key] = osclients.Clients(key, api_info=api_versions)
 
         return cache[key]
 
@@ -146,12 +150,16 @@ class SeekAndDestroy(object):
             if self.admin and (not self.users
                                or self.manager_cls._perform_for_admin_only):
                 manager = self.manager_cls(
-                    admin=self._get_cached_client(self.admin))
+                    admin=self._get_cached_client(
+                        self.admin,
+                        api_versions=self.api_versions))
                 _publish(self.admin, None, manager)
 
             else:
                 visited_tenants = set()
-                admin_client = self._get_cached_client(self.admin)
+                admin_client = self._get_cached_client(
+                    self.admin,
+                    api_versions=self.api_versions)
                 for user in self.users:
                     if (self.manager_cls._tenant_resource
                        and user["tenant_id"] in visited_tenants):
@@ -160,7 +168,9 @@ class SeekAndDestroy(object):
                     visited_tenants.add(user["tenant_id"])
                     manager = self.manager_cls(
                         admin=admin_client,
-                        user=self._get_cached_client(user),
+                        user=self._get_cached_client(
+                            user,
+                            api_versions=self.api_versions),
                         tenant_uuid=user["tenant_id"])
 
                     _publish(self.admin, user, manager)
@@ -176,8 +186,12 @@ class SeekAndDestroy(object):
 
             manager = self.manager_cls(
                 resource=raw_resource,
-                admin=self._get_cached_client(admin, cache=cache),
-                user=self._get_cached_client(user, cache=cache),
+                admin=self._get_cached_client(admin,
+                                              cache=cache,
+                                              api_versions=self.api_versions),
+                user=self._get_cached_client(user,
+                                             cache=cache,
+                                             api_versions=self.api_versions),
                 tenant_uuid=user and user["tenant_id"])
 
             self._delete_single_resource(manager)
@@ -248,7 +262,8 @@ def find_resource_managers(names=None, admin_required=None):
     return resource_managers
 
 
-def cleanup(names=None, admin_required=None, admin=None, users=None):
+def cleanup(names=None, admin_required=None, admin=None, users=None,
+            api_versions=None):
     """Generic cleaner.
 
     This method goes through all plugins. Filter those and left only plugins
@@ -279,4 +294,4 @@ def cleanup(names=None, admin_required=None, admin=None, users=None):
         LOG.debug("Cleaning up %(service)s %(resource)s objects" %
                   {"service": manager._service,
                    "resource": manager._resource})
-        SeekAndDestroy(manager, admin, users).exterminate()
+        SeekAndDestroy(manager, admin, users, api_versions).exterminate()

tests/unit/plugins/openstack/cleanup/test_manager.py
@@ -32,6 +32,7 @@ class SeekAndDestroyTestCase(test.TestCase):
         self.assertIsNone(manager.SeekAndDestroy._get_cached_client(None))
 
         users = [{"credential": "a"}, {"credential": "b"}]
+
         cache = {}
 
         self.assertEqual(
@@ -45,6 +46,45 @@ class SeekAndDestroyTestCase(test.TestCase):
         self.assertNotEqual(
             manager.SeekAndDestroy._get_cached_client(users[0], cache=cache),
             manager.SeekAndDestroy._get_cached_client(users[1], cache=cache))
+
+    @mock.patch("%s.osclients.Clients" % BASE,
+                side_effect=[mock.MagicMock(), mock.MagicMock()])
+    def test__get_cached_client_with_api_versions(self, mock_clients):
+        self.assertIsNone(manager.SeekAndDestroy._get_cached_client(None))
+
+        user = {"credential": "a"}
+        api_versions = [
+            {"cinder": {"version": "1", "service_type": "volume"}},
+            {"cinder": {"version": "2", "service_type": "volumev2"}}
+        ]
+
+        cache = {}
+
+        self.assertEqual(
+            manager.SeekAndDestroy._get_cached_client(
+                user, cache=cache, api_versions=api_versions[0]),
+            manager.SeekAndDestroy._get_cached_client(
+                user, cache=cache, api_versions=api_versions[0]))
+
+        self.assertEqual(
+            manager.SeekAndDestroy._get_cached_client(
+                user,
+                cache=cache,
+                api_versions=api_versions[1]),
+            manager.SeekAndDestroy._get_cached_client(
+                user,
+                cache=cache,
+                api_versions=api_versions[1]))
+
+        self.assertNotEqual(
+            manager.SeekAndDestroy._get_cached_client(
+                user,
+                cache=cache,
+                api_versions=api_versions[0]),
+            manager.SeekAndDestroy._get_cached_client(
+                user,
+                cache=cache,
+                api_versions=api_versions[1]))
 
     @mock.patch("%s.LOG" % BASE)
     def test__delete_single_resource(self, mock_log):
@@ -116,7 +156,8 @@ class SeekAndDestroyTestCase(test.TestCase):
 
         queue = []
         publish(queue)
-        mock__get_cached_client.assert_called_once_with(admin)
+        mock__get_cached_client.assert_called_once_with(admin,
+                                                        api_versions=None)
         mock_mgr.assert_called_once_with(
             admin=mock__get_cached_client.return_value)
         self.assertEqual(queue, [(admin, None, x) for x in range(1, 4)])
@@ -131,7 +172,8 @@ class SeekAndDestroyTestCase(test.TestCase):
 
         queue = []
         publish(queue)
-        mock__get_cached_client.assert_called_once_with(admin)
+        mock__get_cached_client.assert_called_once_with(admin,
+                                                        api_versions=None)
         mock_mgr.assert_called_once_with(
             admin=mock__get_cached_client.return_value)
         self.assertEqual(queue, [(admin, None, x) for x in range(1, 4)])
@@ -164,9 +206,9 @@ class SeekAndDestroyTestCase(test.TestCase):
             mock.call().list()
         ])
         mock__get_cached_client.assert_has_calls([
-            mock.call(admin),
-            mock.call(users[0]),
-            mock.call(users[1])
+            mock.call(admin, api_versions=None),
+            mock.call(users[0], api_versions=None),
+            mock.call(users[1], api_versions=None)
         ])
         expected_queue = [(admin, users[0], x) for x in range(1, 4)]
         expected_queue += [(admin, users[1], x) for x in range(4, 6)]
@@ -202,9 +244,9 @@ class SeekAndDestroyTestCase(test.TestCase):
             mock.call().list()
         ])
         mock__get_cached_client.assert_has_calls([
-            mock.call(None),
-            mock.call(users[0]),
-            mock.call(users[2])
+            mock.call(None, api_versions=None),
+            mock.call(users[0], api_versions=None),
+            mock.call(users[2], api_versions=None)
         ])
         self.assertEqual(queue, [(None, users[0], x) for x in range(1, 4)])
 
@@ -227,8 +269,8 @@ class SeekAndDestroyTestCase(test.TestCase):
             user=mock__get_cached_client.return_value,
             tenant_uuid=user1["tenant_id"])
         mock__get_cached_client.assert_has_calls([
-            mock.call(admin, cache=cache),
-            mock.call(user1, cache=cache)
+            mock.call(admin, cache=cache, api_versions=None),
+            mock.call(user1, cache=cache, api_versions=None)
         ])
         mock__delete_single_resource.assert_called_once_with(
             mock_mgr.return_value)
@@ -245,8 +287,8 @@ class SeekAndDestroyTestCase(test.TestCase):
             tenant_uuid=None)
 
         mock__get_cached_client.assert_has_calls([
-            mock.call(admin, cache=cache),
-            mock.call(None, cache=cache)
+            mock.call(admin, cache=cache, api_versions=None),
+            mock.call(None, cache=cache, api_versions=None)
         ])
         mock__delete_single_resource.assert_called_once_with(
             mock_mgr.return_value)
@@ -344,11 +386,42 @@ class ResourceManagerTestCase(test.TestCase):
 
         mock_seek_and_destroy.assert_has_calls([
             mock.call(
-                mock_find_resource_managers.return_value[0], "admin", ["user"]
+                mock_find_resource_managers.return_value[0], "admin",
+                ["user"], None
             ),
             mock.call().exterminate(),
             mock.call(
-                mock_find_resource_managers.return_value[1], "admin", ["user"]
+                mock_find_resource_managers.return_value[1], "admin",
+                ["user"], None
+            ),
+            mock.call().exterminate()
+        ])
+
+    @mock.patch("%s.SeekAndDestroy" % BASE)
+    @mock.patch("%s.find_resource_managers" % BASE,
+                return_value=[mock.MagicMock(), mock.MagicMock()])
+    def test_cleanup_with_api_versions(self,
+                                       mock_find_resource_managers,
+                                       mock_seek_and_destroy):
+        manager.cleanup(names=["a", "b"], admin_required=True,
+                        admin="admin", users=["user"],
+                        api_versions={"cinder": {
+                            "version": "1", "service_type": "volume"
+                        }})
+
+        mock_find_resource_managers.assert_called_once_with(["a", "b"], True)
+
+        mock_seek_and_destroy.assert_has_calls([
+            mock.call(
+                mock_find_resource_managers.return_value[0], "admin",
+                ["user"],
+                {"cinder": {"service_type": "volume", "version": "1"}}
+            ),
+            mock.call().exterminate(),
+            mock.call(
+                mock_find_resource_managers.return_value[1], "admin",
+                ["user"],
+                {"cinder": {"service_type": "volume", "version": "1"}}
             ),
             mock.call().exterminate()
         ])

rally/plugins/openstack/context/cleanup/__init__.py
@@ -64,10 +64,12 @@ class AdminCleanup(CleanupMixin, context.Context):
 
     @logging.log_task_wrapper(LOG.info, _("admin resources cleanup"))
     def cleanup(self):
-        manager.cleanup(names=self.config,
-                        admin_required=True,
-                        admin=self.context["admin"],
-                        users=self.context.get("users", []))
+        manager.cleanup(
+            names=self.config,
+            admin_required=True,
+            admin=self.context["admin"],
+            users=self.context.get("users", []),
+            api_versions=self.context["config"].get("api_versions"))
 
 
 # NOTE(amaretskiy): Set maximum order to run this last
@@ -89,6 +91,9 @@ class UserCleanup(CleanupMixin, context.Context):
 
     @logging.log_task_wrapper(LOG.info, _("user resources cleanup"))
     def cleanup(self):
-        manager.cleanup(names=self.config,
-                        admin_required=False,
-                        users=self.context.get("users", []))
+        manager.cleanup(
+            names=self.config,
+            admin_required=False,
+            users=self.context.get("users", []),
+            api_versions=self.context["config"].get("api_versions")
+        )

commit aac1c5ffe42e8bab1f7535e532541dfff58a7b2c

Overhaul SeekAndDestroy._get_cached_client()
b331e10 introduced a bug that caused every call to
_get_cached_client() that hit the cache to return a completely bogus
Clients object. Luckily the call signature of _get_cached_client() was
a disaster, so use of the cache was spotty and inconsistent. This
commit:

- Makes use of the cache mandatory and consistent;
- Makes use of the api_versions data passed to SeekAndDestroy mandatory;
- Shares the cache between all SeekAndDestroy objects.

The bug itself was fixed by b6a5a034.

Change-Id: I2bbfd866b03f986da528f6ec614b0b986bb9aebe

rally/plugins/openstack/cleanup/manager.py
@@ -28,6 +28,7 @@ LOG = logging.getLogger(__name__)
 
 
 class SeekAndDestroy(object):
+    cache = {}
 
     def __init__(self, manager_cls, admin, users, api_versions=None):
         """Resource deletion class.
@@ -38,31 +39,26 @@ class SeekAndDestroy(object):
         :param manager_cls: subclass of base.ResourceManager
         :param admin: admin credential like in context["admin"]
         :param users: users credentials like in context["users"]
+        :param api_versions: dict of client API versions
         """
         self.manager_cls = manager_cls
         self.admin = admin
         self.users = users or []
         self.api_versions = api_versions
 
-    @staticmethod
-    def _get_cached_client(user, cache=None, api_versions=None):
+    def _get_cached_client(self, user):
         """Simplifies initialization and caching OpenStack clients."""
-
         if not user:
             return None
 
-        if not isinstance(cache, dict):
-            return osclients.Clients(user["credential"], api_info=api_versions)
-
-        if api_versions:
-            key = str((user["credential"], sorted(api_versions.items())))
+        if self.api_versions:
+            key = str((user["credential"], sorted(self.api_versions.items())))
         else:
             key = user["credential"]
-        if key not in cache:
-            cache[key] = osclients.Clients(user["credential"],
-                                           api_info=api_versions)
-
-        return cache[key]
+        if key not in self.cache:
+            self.cache[key] = osclients.Clients(
+                user["credential"], api_info=self.api_versions)
+        return self.cache[key]
 
     def _delete_single_resource(self, resource):
         """Safe resource deletion with retries and timeouts.
@@ -151,16 +147,12 @@ class SeekAndDestroy(object):
             if self.admin and (not self.users
                                or self.manager_cls._perform_for_admin_only):
                 manager = self.manager_cls(
-                    admin=self._get_cached_client(
-                        self.admin,
-                        api_versions=self.api_versions))
+                    admin=self._get_cached_client(self.admin))
                 _publish(self.admin, None, manager)
 
             else:
                 visited_tenants = set()
-                admin_client = self._get_cached_client(
-                    self.admin,
-                    api_versions=self.api_versions)
+                admin_client = self._get_cached_client(self.admin)
                 for user in self.users:
                     if (self.manager_cls._tenant_resource
                        and user["tenant_id"] in visited_tenants):
@@ -169,9 +161,7 @@ class SeekAndDestroy(object):
                     visited_tenants.add(user["tenant_id"])
                     manager = self.manager_cls(
                         admin=admin_client,
-                        user=self._get_cached_client(
-                            user,
-                            api_versions=self.api_versions),
+                        user=self._get_cached_client(user),
                         tenant_uuid=user["tenant_id"])
 
                     _publish(self.admin, user, manager)
@@ -187,12 +177,8 @@ class SeekAndDestroy(object):
 
             manager = self.manager_cls(
                 resource=raw_resource,
-                admin=self._get_cached_client(admin,
-                                              cache=cache,
-                                              api_versions=self.api_versions),
-                user=self._get_cached_client(user,
-                                             cache=cache,
-                                             api_versions=self.api_versions),
+                admin=self._get_cached_client(admin),
+                user=self._get_cached_client(user),
                 tenant_uuid=user and user["tenant_id"])
 
             self._delete_single_resource(manager)

tests/unit/plugins/openstack/cleanup/test_manager.py
@@ -26,61 +26,91 @@ BASE = "rally.plugins.openstack.cleanup.manager"
 
 class SeekAndDestroyTestCase(test.TestCase):
 
-    def test__get_cached_client(self):
-        self.assertIsNone(manager.SeekAndDestroy._get_cached_client(None))
+    def setUp(self):
+        super(SeekAndDestroyTestCase, self).setUp()
+        # clear out the client cache
+        manager.SeekAndDestroy.cache = {}
+
+    @mock.patch("%s.osclients.Clients" % BASE,
+                side_effect=[mock.MagicMock(), mock.MagicMock()])
+    def test__get_cached_client(self, mock_clients):
+        destroyer = manager.SeekAndDestroy(None, None, None)
+
+        self.assertIsNone(destroyer._get_cached_client(None))
 
         users = [{"credential": "a"}, {"credential": "b"}]
 
-        cache = {}
+        self.assertEqual(destroyer._get_cached_client(users[0]),
+                         destroyer._get_cached_client(users[0]))
+        # ensure that cache is used
+        self.assertItemsEqual(mock_clients.call_args_list,
+                              [mock.call("a", api_info=None)])
 
-        self.assertEqual(
-            manager.SeekAndDestroy._get_cached_client(users[0], cache=cache),
-            manager.SeekAndDestroy._get_cached_client(users[0], cache=cache))
+        mock_clients.reset_mock()
+        self.assertEqual(destroyer._get_cached_client(users[1]),
+                         destroyer._get_cached_client(users[1]))
+        self.assertItemsEqual(mock_clients.call_args_list,
+                              [mock.call("b", api_info=None)])
 
-        self.assertEqual(
-            manager.SeekAndDestroy._get_cached_client(users[1], cache=cache),
-            manager.SeekAndDestroy._get_cached_client(users[1], cache=cache))
+        mock_clients.reset_mock()
+        self.assertNotEqual(destroyer._get_cached_client(users[0]),
+                            destroyer._get_cached_client(users[1]))
+        self.assertFalse(mock_clients.called)
 
-        self.assertNotEqual(
-            manager.SeekAndDestroy._get_cached_client(users[0], cache=cache),
-            manager.SeekAndDestroy._get_cached_client(users[1], cache=cache))
-
-    def test__get_cached_client_with_api_versions(self):
-        self.assertIsNone(manager.SeekAndDestroy._get_cached_client(None))
+    @mock.patch("%s.osclients.Clients" % BASE,
+                side_effect=[mock.MagicMock(), mock.MagicMock()])
+    def test__get_cached_client_shared_cache(self, mock_clients):
+        # ensure that cache is shared between SeekAndDestroy objects
+        destroyer1 = manager.SeekAndDestroy(None, None, None)
+        destroyer2 = manager.SeekAndDestroy(None, None, None)
 
         user = {"credential": "a"}
+
+        self.assertEqual(destroyer1._get_cached_client(user),
+                         destroyer2._get_cached_client(user))
+        self.assertItemsEqual(mock_clients.call_args_list,
+                              [mock.call("a", api_info=None)])
+
+    @mock.patch("%s.osclients.Clients" % BASE,
+                side_effect=[mock.MagicMock(), mock.MagicMock()])
+    def test__get_cached_client_shared_cache_api_versions(self, mock_clients):
+        # ensure that cache is shared between SeekAndDestroy objects
+        # with matching api_versions dicts
+        api_versions = {"cinder": {"version": "1", "service_type": "volume"}}
+
+        destroyer1 = manager.SeekAndDestroy(None, None, None,
+                                            api_versions=api_versions)
+        destroyer2 = manager.SeekAndDestroy(None, None, None,
+                                            api_versions=api_versions)
+
+        user = {"credential": "a"}
+
+        self.assertEqual(destroyer1._get_cached_client(user),
+                         destroyer2._get_cached_client(user))
+        self.assertItemsEqual(mock_clients.call_args_list,
+                              [mock.call("a", api_info=api_versions)])
+
+    @mock.patch("%s.osclients.Clients" % BASE,
+                side_effect=[mock.MagicMock(), mock.MagicMock()])
+    def test__get_cached_client_no_cache_api_versions(self, mock_clients):
+        # ensure that cache is not shared between SeekAndDestroy
+        # objects with different api_versions dicts
         api_versions = [
             {"cinder": {"version": "1", "service_type": "volume"}},
             {"cinder": {"version": "2", "service_type": "volumev2"}}
         ]
 
-        cache = {}
+        destroyer1 = manager.SeekAndDestroy(None, None, None,
+                                            api_versions=api_versions[0])
+        destroyer2 = manager.SeekAndDestroy(None, None, None,
+                                            api_versions=api_versions[1])
+        user = {"credential": "a"}
 
-        self.assertEqual(
-            manager.SeekAndDestroy._get_cached_client(
-                user, cache=cache, api_versions=api_versions[0]),
-            manager.SeekAndDestroy._get_cached_client(
-                user, cache=cache, api_versions=api_versions[0]))
-
-        self.assertEqual(
-            manager.SeekAndDestroy._get_cached_client(
-                user,
-                cache=cache,
-                api_versions=api_versions[1]),
-            manager.SeekAndDestroy._get_cached_client(
-                user,
-                cache=cache,
-                api_versions=api_versions[1]))
-
-        self.assertNotEqual(
-            manager.SeekAndDestroy._get_cached_client(
-                user,
-                cache=cache,
-                api_versions=api_versions[0]),
-            manager.SeekAndDestroy._get_cached_client(
-                user,
-                cache=cache,
-                api_versions=api_versions[1]))
+        self.assertNotEqual(destroyer1._get_cached_client(user),
+                            destroyer2._get_cached_client(user))
+        self.assertItemsEqual(mock_clients.call_args_list,
+                              [mock.call("a", api_info=api_versions[0]),
+                               mock.call("a", api_info=api_versions[1])])
 
     @mock.patch("%s.LOG" % BASE)
     def test__delete_single_resource(self, mock_log):
@@ -152,8 +182,7 @@ class SeekAndDestroyTestCase(test.TestCase):
 
         queue = []
         publish(queue)
-        mock__get_cached_client.assert_called_once_with(admin,
-                                                        api_versions=None)
+        mock__get_cached_client.assert_called_once_with(admin)
         mock_mgr.assert_called_once_with(
             admin=mock__get_cached_client.return_value)
         self.assertEqual(queue, [(admin, None, x) for x in range(1, 4)])
@@ -168,8 +197,7 @@ class SeekAndDestroyTestCase(test.TestCase):
 
         queue = []
         publish(queue)
-        mock__get_cached_client.assert_called_once_with(admin,
-                                                        api_versions=None)
+        mock__get_cached_client.assert_called_once_with(admin)
         mock_mgr.assert_called_once_with(
             admin=mock__get_cached_client.return_value)
         self.assertEqual(queue, [(admin, None, x) for x in range(1, 4)])
@@ -202,9 +230,9 @@ class SeekAndDestroyTestCase(test.TestCase):
             mock.call().list()
         ])
         mock__get_cached_client.assert_has_calls([
-            mock.call(admin, api_versions=None),
-            mock.call(users[0], api_versions=None),
-            mock.call(users[1], api_versions=None)
+            mock.call(admin),
+            mock.call(users[0]),
+            mock.call(users[1])
         ])
         expected_queue = [(admin, users[0], x) for x in range(1, 4)]
         expected_queue += [(admin, users[1], x) for x in range(4, 6)]
@@ -240,9 +268,9 @@ class SeekAndDestroyTestCase(test.TestCase):
             mock.call().list()
         ])
         mock__get_cached_client.assert_has_calls([
-            mock.call(None, api_versions=None),
-            mock.call(users[0], api_versions=None),
-            mock.call(users[2], api_versions=None)
+            mock.call(None),
+            mock.call(users[0]),
+            mock.call(users[2])
         ])
         self.assertEqual(queue, [(None, users[0], x) for x in range(1, 4)])
 
@@ -265,8 +293,8 @@ class SeekAndDestroyTestCase(test.TestCase):
             user=mock__get_cached_client.return_value,
             tenant_uuid=user1["tenant_id"])
         mock__get_cached_client.assert_has_calls([
-            mock.call(admin, cache=cache, api_versions=None),
-            mock.call(user1, cache=cache, api_versions=None)
+            mock.call(admin),
+            mock.call(user1)
         ])
         mock__delete_single_resource.assert_called_once_with(
             mock_mgr.return_value)
@@ -283,8 +311,8 @@ class SeekAndDestroyTestCase(test.TestCase):
             tenant_uuid=None)
 
         mock__get_cached_client.assert_has_calls([
-            mock.call(admin, cache=cache, api_versions=None),
-            mock.call(None, cache=cache, api_versions=None)
+            mock.call(admin),
+            mock.call(None)
         ])
         mock__delete_single_resource.assert_called_once_with(
             mock_mgr.return_value)