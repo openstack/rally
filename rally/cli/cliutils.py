dd0bdf50f39e818c734b6f89441ff7d35adf3999
Sindhur <smalleni@redhat.com>
2016-05-17 14:26:08 -0400

    Fix typo in rally/cli/cliutils.py
    In the error message "Make a sure that connection string in rally.conf is
    proper:", a shouldn't be present. This commit simply fixes that.
    
    Change-Id: If61a968c739157e1a8dcc7324776ffea1d897ea4

--- rally/cli/cliutils.py
+++ rally/cli/cliutils.py
@@ -592,7 +592,7 @@ def run(argv, categories):
             LOG.exception(e)
         print(e)
         print("Looks like Rally can't connect to its DB.")
-        print("Make a sure that connection string in rally.conf is proper:")
+        print("Make sure that connection string in rally.conf is proper:")
         print(CONF.database.connection)
         return 1
     except Exception:
