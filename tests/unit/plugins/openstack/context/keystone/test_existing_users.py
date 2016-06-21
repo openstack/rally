42c2e0cba5642a2076f37de05f665305edbf06e2
Illia Khudoshyn <ikhudoshyn@mirantis.com>
2016-06-21 15:24:48 +0000

    Fix 'existing_users' context against keystone v3
    And restore disabled tests
    
    Change-Id: Iea75a9733e8f23aafb19171ae6eb706f8a980870

--- tests/unit/plugins/openstack/context/keystone/test_existing_users.py
+++ tests/unit/plugins/openstack/context/keystone/test_existing_users.py
@@ -24,10 +24,10 @@ class ExistingUserTestCase(test.TestCase):
 
     @mock.patch("%s.keystone.existing_users.osclients.Clients" % CTX)
     @mock.patch("%s.keystone.existing_users.objects.Credential" % CTX)
-    def test_setup(self, mock_credential, mock_clients):
-        user1 = mock.MagicMock(tenant_id="1")
-        user2 = mock.MagicMock(tenant_id="1")
-        user3 = mock.MagicMock(tenant_id="2")
+    def test_setup_keystonev2(self, mock_credential, mock_clients):
+        user1 = mock.MagicMock(tenant_id="1", version="2.0")
+        user2 = mock.MagicMock(tenant_id="1", version="2.0")
+        user3 = mock.MagicMock(tenant_id="2", version="2.0")
 
         mock_clients.return_value.keystone.side_effect = [
             user1, user2, user3
@@ -56,6 +56,44 @@ class ExistingUserTestCase(test.TestCase):
         self.assertEqual({"id": "1", "name": user1.tenant_name},
                          context["tenants"]["1"])
         self.assertEqual({"id": "2", "name": user3.tenant_name},
+                         context["tenants"]["2"])
+
+    @mock.patch("%s.keystone.existing_users.osclients.Clients" % CTX)
+    @mock.patch("%s.keystone.existing_users.objects.Credential" % CTX)
+    def test_setup_keystonev3(self, mock_credential, mock_clients):
+        user1 = mock.MagicMock(project_id="1", version="3")
+        user2 = mock.MagicMock(project_id="1", version="3")
+        user3 = mock.MagicMock(project_id=None, version="3")
+
+        user3.get_project_id.return_value = "2"
+
+        mock_clients.return_value.keystone.side_effect = [
+            user1, user2, user3
+        ]
+
+        context = {
+            "task": mock.MagicMock(),
+            "config": {
+                "existing_users": [user1, user2, user3]
+            }
+        }
+        existing_users.ExistingUsers(context).setup()
+
+        self.assertIn("users", context)
+        self.assertIn("tenants", context)
+        self.assertEqual(3, len(context["users"]))
+        self.assertEqual(
+            {
+                "id": user1.user_id,
+                "credential": mock_credential.return_value,
+                "tenant_id": "1"
+            },
+            context["users"][0]
+        )
+        self.assertEqual(["1", "2"], sorted(context["tenants"].keys()))
+        self.assertEqual({"id": "1", "name": user1.project_name},
+                         context["tenants"]["1"])
+        self.assertEqual({"id": "2", "name": user3.project_name},
                          context["tenants"]["2"])
 
     def test_cleanup(self):
