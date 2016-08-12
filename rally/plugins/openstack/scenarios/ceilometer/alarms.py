1783156ccf30e72028988ac30b5b4d3bd392b4ab
Anton Staroverov <astaroverov@mirantis.com>
2016-08-12 12:14:43 +0300

    Moving of Ceilometer Alarms scenarios into separate classes
    Since current scenario implementation transforms method to class
    at runtime, it is reasonable to have class-based scenario
    implementation which will be much simpler to use behind the scenes.
    
    This class should be based on Scenario and do not break compatibility.
    The only required method is run() which is actually a body of scenario.
    
    Change-Id: I1f1ef261942bcaf7617a5f10f05a74fae35c53f2

--- rally/plugins/openstack/scenarios/ceilometer/alarms.py
+++ rally/plugins/openstack/scenarios/ceilometer/alarms.py
@@ -17,14 +17,16 @@ from rally.plugins.openstack import scenario
 from rally.plugins.openstack.scenarios.ceilometer import utils as ceiloutils
 from rally.task import validation
 
+"""Benchmark scenarios for Ceilometer Alarms API."""
 
-class CeilometerAlarms(ceiloutils.CeilometerScenario):
-    """Benchmark scenarios for Ceilometer Alarms API."""
 
-    @validation.required_services(consts.Service.CEILOMETER)
-    @validation.required_openstack(users=True)
-    @scenario.configure(context={"cleanup": ["ceilometer"]})
-    def create_alarm(self, meter_name, threshold, **kwargs):
+@validation.required_services(consts.Service.CEILOMETER)
+@validation.required_openstack(users=True)
+@scenario.configure(context={"cleanup": ["ceilometer"]},
+                    name="CeilometerAlarms.create_alarm")
+class CreateAlarm(ceiloutils.CeilometerScenario):
+
+    def run(self, meter_name, threshold, **kwargs):
         """Create an alarm.
 
         This scenarios test POST /v2/alarms.
@@ -36,22 +38,30 @@ class CeilometerAlarms(ceiloutils.CeilometerScenario):
         :param threshold: specifies alarm threshold
         :param kwargs: specifies optional arguments for alarm creation.
         """
+
         self._create_alarm(meter_name, threshold, kwargs)
 
-    @validation.required_services(consts.Service.CEILOMETER)
-    @validation.required_openstack(users=True)
-    @scenario.configure()
-    def list_alarms(self):
+
+@validation.required_services(consts.Service.CEILOMETER)
+@validation.required_openstack(users=True)
+@scenario.configure(name="CeilometerAlarms.list_alarms")
+class ListAlarms(ceiloutils.CeilometerScenario):
+
+    def run(self):
         """Fetch all alarms.
 
         This scenario fetches list of all alarms using GET /v2/alarms.
         """
         self._list_alarms()
 
-    @validation.required_services(consts.Service.CEILOMETER)
-    @validation.required_openstack(users=True)
-    @scenario.configure(context={"cleanup": ["ceilometer"]})
-    def create_and_list_alarm(self, meter_name, threshold, **kwargs):
+
+@validation.required_services(consts.Service.CEILOMETER)
+@validation.required_openstack(users=True)
+@scenario.configure(context={"cleanup": ["ceilometer"]},
+                    name="CeilometerAlarms.create_and_list_alarm")
+class CreateAndListAlarm(ceiloutils.CeilometerScenario):
+
+    def run(self, meter_name, threshold, **kwargs):
         """Create and get the newly created alarm.
 
         This scenarios test GET /v2/alarms/(alarm_id)
@@ -68,10 +78,14 @@ class CeilometerAlarms(ceiloutils.CeilometerScenario):
         alarm = self._create_alarm(meter_name, threshold, kwargs)
         self._list_alarms(alarm.alarm_id)
 
-    @validation.required_services(consts.Service.CEILOMETER)
-    @validation.required_openstack(users=True)
-    @scenario.configure(context={"cleanup": ["ceilometer"]})
-    def create_and_update_alarm(self, meter_name, threshold, **kwargs):
+
+@validation.required_services(consts.Service.CEILOMETER)
+@validation.required_openstack(users=True)
+@scenario.configure(context={"cleanup": ["ceilometer"]},
+                    name="CeilometerAlarms.create_and_update_alarm")
+class CreateAndUpdateAlarm(ceiloutils.CeilometerScenario):
+
+    def run(self, meter_name, threshold, **kwargs):
         """Create and update the newly created alarm.
 
         This scenarios test PUT /v2/alarms/(alarm_id)
@@ -88,10 +102,14 @@ class CeilometerAlarms(ceiloutils.CeilometerScenario):
         alarm_dict_diff = {"description": "Changed Test Description"}
         self._update_alarm(alarm.alarm_id, alarm_dict_diff)
 
-    @validation.required_services(consts.Service.CEILOMETER)
-    @validation.required_openstack(users=True)
-    @scenario.configure(context={"cleanup": ["ceilometer"]})
-    def create_and_delete_alarm(self, meter_name, threshold, **kwargs):
+
+@validation.required_services(consts.Service.CEILOMETER)
+@validation.required_openstack(users=True)
+@scenario.configure(context={"cleanup": ["ceilometer"]},
+                    name="CeilometerAlarms.create_and_delete_alarm")
+class CreateAndDeleteAlarm(ceiloutils.CeilometerScenario):
+
+    def run(self, meter_name, threshold, **kwargs):
         """Create and delete the newly created alarm.
 
         This scenarios test DELETE /v2/alarms/(alarm_id)
@@ -107,11 +125,14 @@ class CeilometerAlarms(ceiloutils.CeilometerScenario):
         alarm = self._create_alarm(meter_name, threshold, kwargs)
         self._delete_alarm(alarm.alarm_id)
 
-    @validation.required_services(consts.Service.CEILOMETER)
-    @validation.required_openstack(users=True)
-    @scenario.configure(context={"cleanup": ["ceilometer"]})
-    def create_alarm_and_get_history(self, meter_name, threshold, state,
-                                     timeout=60, **kwargs):
+
+@validation.required_services(consts.Service.CEILOMETER)
+@validation.required_openstack(users=True)
+@scenario.configure(context={"cleanup": ["ceilometer"]},
+                    name="CeilometerAlarms.create_alarm_and_get_history")
+class CreateAlarmAndGetHistory(ceiloutils.CeilometerScenario):
+
+    def run(self, meter_name, threshold, state, timeout=60, **kwargs):
         """Create an alarm, get and set the state and get the alarm history.
 
          This scenario makes following queries:
