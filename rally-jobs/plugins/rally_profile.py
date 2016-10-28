dd9c973b89b09cd0b1f74a2dc84d696369fa1eae
Anton Studenov <astudenov@mirantis.com>
2016-10-28 20:01:30 +0300

    [Rally profile] Performance of creating atomic timers
    This patch adds scenario that calculates creation of
    specified number of atomic timers.
    
    Change-Id: I68ba4e7d09fae29f3edb4630d04ad6b981ecbed1

--- rally-jobs/plugins/rally_profile.py
+++ rally-jobs/plugins/rally_profile.py
@@ -30,3 +30,23 @@ class GenerateNamesInAtomic(scenario.Scenario, utils.RandomNameGeneratorMixin):
         with atomic.ActionTimer(self, "generate_%s_names" % number_of_names):
             for i in range(number_of_names):
                 self.generate_random_name()
+
+
+@scenario.configure(name="RallyProfile.calculate_atomic")
+class CalculateAtomic(scenario.Scenario, utils.RandomNameGeneratorMixin):
+
+    def run(self, number_of_atomics):
+        """Calculate atomic actions.
+
+        :param number_of_atomics: int number of atomics to run
+        """
+        tmp_name = "tmp_actions"
+
+        calc_atomic_name = "calculate_%s_atomics" % number_of_atomics
+        with atomic.ActionTimer(self, calc_atomic_name):
+            for _ in range(number_of_atomics):
+                with atomic.ActionTimer(self, tmp_name):
+                    pass
+
+        self._atomic_actions = {
+            calc_atomic_name: self._atomic_actions[calc_atomic_name]}
