d02d4f339979d7127890f7cb1030490c9f64676c
Andrey Kurilin <andr.kurilin@gmail.com>
2016-12-19 19:53:08 +0200

    [verification] Prepare reports to new structure
    Changes in VerificationReport
    
    - add comparison between verifications
    - move processing of 'has_details' to to_html method
    - accept list of verifications in db output format
    
    Co-Authored-By: Yaroslav Lobankov <ylobankov@mirantis.com>
    
    Change-Id: Ifdcce17ff8ffd7522accba8b4db2070fc68cd472

--- rally/ui/report.py
+++ rally/ui/report.py
@@ -15,87 +15,118 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import csv
-import io
+import collections
+import copy
 import json
 import re
-
-from jinja2 import utils as jinja_utils
 
 from rally.ui import utils
 
 
+SKIP_RE = re.compile("Skipped until Bug: ?(?P<bug_number>\d+) is resolved.")
+LP_BUG_LINK = "https://launchpad.net/bugs/%s"
+
+
 class VerificationReport(object):
+    """Generate a report for a verification or a few verifications."""
 
-    SKIP_RE = re.compile(
-        "Skipped until Bug: ?(?P<bug_number>\d+) is resolved.")
-    LP_BUG_LINK = "<a href='https://launchpad.net/bugs/{0}'>{0}</a>"
-
-    def __init__(self, verifications):
-        self._runs = verifications
-        self._uuids = list(verifications.keys())
-
-        # NOTE(amaretskiy): make aggregated list of all tests
+    def __init__(self, verifications_list):
+        verifications = collections.OrderedDict()
         tests = {}
-        for uuid, verification in self._runs.items():
-            for name, test in verification["tests"].items():
-                if name not in tests:
-                    # NOTE(ylobankov): It is more convenient to see resource
-                    #                  ID at the first place in the report.
-                    tags = sorted(test["tags"], reverse=True,
-                                  key=lambda tag: tag.startswith("id-"))
-                    tests[name] = {"name": name,
-                                   "tags": tags,
-                                   "by_verification": {},
-                                   "has_details": False}
 
-                tests[name]["by_verification"][uuid] = {
-                    "status": test["status"],
-                    "duration": test["duration"],
-                    "details": test["details"]
+        for v in verifications_list:
+            verifications[v.uuid] = {
+                "started_at": v.created_at.strftime("%Y-%m-%d %H:%M:%S"),
+                "finished_at": v.updated_at.strftime("%Y-%m-%d %H:%M:%S"),
+                "status": v.status,
+                "run_args": v.run_args,
+                "tests_count": v.tests_count,
+                "tests_duration": v.tests_duration,
+                "skipped": v.skipped,
+                "success": v.success,
+                "expected_failures": v.expected_failures,
+                "unexpected_success": v.unexpected_success,
+                "failures": v.failures,
+            }
+
+            for test_id, result in v.tests.items():
+                if test_id not in tests:
+                    # NOTE(ylobankov): It is more convenient to see test ID
+                    #                  at the first place in the report.
+                    tags = sorted(result.get("tags", []), reverse=True,
+                                  key=lambda tag: tag.startswith("id-"))
+                    tests[test_id] = {"tags": tags,
+                                      "name": result["name"],
+                                      "by_verification": {}}
+
+                reason = result.get("reason", "")
+                if reason:
+                    match = SKIP_RE.match(reason)
+                    if match:
+                        link = LP_BUG_LINK % match.group("bug_number")
+                        reason = re.sub(match.group("bug_number"), link,
+                                        reason)
+                traceback = result.get("traceback", "")
+                sep = "\n\n" if reason and traceback else ""
+                details = (reason + sep + traceback.strip()) or None
+
+                tests[test_id]["by_verification"][v.uuid] = {
+                    "status": result["status"],
+                    "duration": result["duration"],
+                    "details": details
                 }
 
-                if test["details"]:
-                    tests[name]["has_details"] = True
-
-                    match = self.SKIP_RE.match(test["details"])
-                    if match:
-                        href = self.LP_BUG_LINK.format(
-                            match.group("bug_number"))
-                        test["details"] = re.sub(
-                            match.group("bug_number"), href, test["details"])
-
-                    test["details"] = jinja_utils.escape(test["details"])
-
-        self._tests = list(tests.values())
+        self.report = {"verifications": verifications, "tests": tests}
 
     def to_html(self):
-        """Make HTML report."""
+        """Generate HTML report."""
+        report = copy.deepcopy(self.report)
+        uuids = report["verifications"].keys()
+        show_comparison_note = False
+
+        for test in report["tests"].values():
+            # make as much as possible processing here to reduce processing
+            # at JS side
+            test["has_details"] = False
+            for test_info in test["by_verification"].values():
+                if test_info["details"]:
+                    test["has_details"] = True
+                    break
+
+            durations = []
+            # iter by uuids to store right order for comparison
+            for uuid in uuids:
+                if uuid in test["by_verification"]:
+                    durations.append(test["by_verification"][uuid]["duration"])
+                    if float(durations[-1]) < 0.001:
+                        durations[-1] = "0"
+                        # not to display such little duration in the report
+                        test["by_verification"][uuid]["duration"] = ""
+
+                    if len(durations) > 1 and not (
+                            durations[0] == "0" and durations[-1] == "0"):
+                        # compare result with result of the first verification
+                        diff = float(durations[-1]) - float(durations[0])
+                        result = "%s (" % durations[-1]
+                        if diff >= 0:
+                            result += "+"
+                        result += "%s)" % diff
+                        test["by_verification"][uuid]["duration"] = result
+
+            if not show_comparison_note and len(durations) > 2:
+                # NOTE(andreykurilin): only in case of comparison of more
+                #   than 2 results of the same test we should display a note
+                #   about the comparison strategy
+                show_comparison_note = True
+
         template = utils.get_template("verification/report.html")
-        context = {"uuids": self._uuids, "verifications": self._runs,
-                   "tests": self._tests}
+        context = {"uuids": uuids,
+                   "verifications": report["verifications"],
+                   "tests": report["tests"],
+                   "show_comparison_note": show_comparison_note}
+
         return template.render(data=json.dumps(context), include_libs=False)
 
     def to_json(self, indent=4):
-        """Make JSON report."""
-        return json.dumps(self._tests, indent=indent)
-
-    def to_csv(self, **kwargs):
-        """Make CSV report."""
-        header = ["test name", "tags", "has errors"]
-        for uuid in self._uuids:
-            header.extend(["%s status" % uuid, "%s duration" % uuid])
-        rows = [header]
-        for test in self._tests:
-            row = [test["name"], " ".join(test["tags"])]
-            for uuid in self._uuids:
-                if uuid not in test["by_verification"]:
-                    row.extend([None, None])
-                    continue
-                row.append(test["by_verification"][uuid]["status"])
-                row.append(test["by_verification"][uuid]["duration"])
-            rows.append(row)
-
-        with io.BytesIO() as stream:
-            csv.writer(stream, **kwargs).writerows(rows)
-            return stream.getvalue()
+        """Generate JSON report."""
+        return json.dumps(self.report, indent=indent)
--- tests/unit/ui/test_report.py
+++ tests/unit/ui/test_report.py
@@ -13,93 +13,312 @@
 #    under the License.
 
 import collections
+import datetime as dt
 
 import mock
 
+from rally.common import utils
 from rally.ui import report
 from tests.unit import test
 
 
 class VerificationReportTestCase(test.TestCase):
+    def get_verifications(self):
+        tests_1 = {
+            "some.test.TestCase.test_foo[id=iiiidddd;smoke]":
+                {"name": "some.test.TestCase.test_foo",
+                 "tags": ["smoke", "id"],
+                 "status": "success",
+                 "duration": "8"},
+            "some.test.TestCase.test_skipped":
+                {"name": "some.test.TestCase.test_skipped",
+                 "status": "skip",
+                 "reason": "Skipped until Bug: 666 is resolved.",
+                 "duration": "0"},
+            "some.test.TestCase.test_xfail":
+                {"name": "some.test.TestCase.test_xfail",
+                 "status": "xfail",
+                 "reason": "something",
+                 "traceback": "HEEELP",
+                 "duration": "3"}
+        }
+        tests_2 = {
+            "some.test.TestCase.test_foo[id=iiiidddd;smoke]":
+                {"name": "some.test.TestCase.test_foo",
+                 "tags": ["smoke", "id"],
+                 "status": "success",
+                 "duration": "8"},
+            "some.test.TestCase.test_failed":
+                {"name": "some.test.TestCase.test_failed",
+                 "status": "fail",
+                 "traceback": "HEEEEEEELP",
+                 "duration": "8"},
+            "some.test.TestCase.test_skipped":
+                {"name": "some.test.TestCase.test_skipped",
+                 "status": "skip",
+                 "reason": "Skipped until Bug: 666 is resolved.",
+                 "duration": "0"},
+            "some.test.TestCase.test_xfail":
+                {"name": "some.test.TestCase.test_xfail",
+                 "status": "xfail",
+                 "reason": "something",
+                 "traceback": "HEEELP",
+                 "duration": "4"}
+        }
+        tests_3 = {
+            "some.test.TestCase.test_foo[id=iiiidddd;smoke]":
+                {"name": "some.test.TestCase.test_foo",
+                 "tags": ["smoke", "id"],
+                 "status": "success",
+                 "duration": "8"},
+            "some.test.TestCase.test_failed":
+                {"name": "some.test.TestCase.test_failed",
+                 "status": "fail",
+                 "traceback": "HEEEEEEELP",
+                 "duration": "7"},
+            "some.test.TestCase.test_skipped":
+                {"name": "some.test.TestCase.test_skipped",
+                 "status": "skip",
+                 "reason": "Skipped until Bug: 666 is resolved.",
+                 "duration": "0"},
+            "some.test.TestCase.test_xfail":
+                {"name": "some.test.TestCase.test_xfail",
+                 "status": "xfail",
+                 "reason": "something",
+                 "traceback": "HEEELP",
+                 "duration": "3"}
+        }
 
-    def gen_instance(self, runs=None, uuids=None, tests=None):
-        ins = report.VerificationReport({})
-        ins._runs = runs or {}
-        ins._uuids = uuids or list(ins._runs.keys())
-        ins._tests = tests or []
-        return ins
+        return [
+            utils.Struct(uuid="foo-bar-1",
+                         created_at=dt.datetime(2001, 1, 1),
+                         updated_at=dt.datetime(2001, 1, 2),
+                         status="finished",
+                         run_args="set_name=compute",
+                         tests_duration=1.111,
+                         tests_count=9,
+                         skipped=0,
+                         success=3,
+                         expected_failures=3,
+                         unexpected_success=2,
+                         failures=1,
+                         tests=tests_1),
+            utils.Struct(uuid="foo-bar-2",
+                         created_at=dt.datetime(2002, 1, 1),
+                         updated_at=dt.datetime(2002, 1, 2),
+                         status="finished",
+                         run_args="set_name=full",
+                         tests_duration=22.222,
+                         tests_count=99,
+                         skipped=0,
+                         success=33,
+                         expected_failures=33,
+                         unexpected_success=22,
+                         failures=11,
+                         tests=tests_2),
+            utils.Struct(uuid="foo-bar-3",
+                         created_at=dt.datetime(2003, 1, 1),
+                         updated_at=dt.datetime(2003, 1, 2),
+                         status="finished",
+                         run_args="set_name=full",
+                         tests_duration=33.333,
+                         tests_count=99,
+                         skipped=0,
+                         success=33,
+                         expected_failures=33,
+                         unexpected_success=22,
+                         failures=11,
+                         tests=tests_3)
+        ]
 
-    def test___init__(self):
-        verifications = collections.OrderedDict([
-            ("a_uuid", {
-                "tests": {"spam": {"status": "fail",
-                                   "duration": 4.2,
-                                   "details": "Some error",
-                                   "tags": ["a-tag", "id-tag", "z-tag"]}}}),
-            ("b_uuid", {
-                "tests": {"foo": {"status": "success", "duration": 0,
-                                  "details": None,
-                                  "tags": ["a-tag", "id-tag", "z-tag"]},
-                          "bar": {"status": "skip", "duration": 4.2,
-                                  "details": None,
-                                  "tags": ["a-tag", "id-tag", "z-tag"]}}})])
-        ins = report.VerificationReport(verifications)
-        self.assertEqual(verifications, ins._runs)
-        self.assertEqual(["a_uuid", "b_uuid"], ins._uuids)
-        tests = [
-            {"has_details": False, "by_verification": {
-                "b_uuid": {"duration": 4.2, "status": "skip",
-                           "details": None}},
-             "name": "bar", "tags": ["id-tag", "a-tag", "z-tag"]},
-            {"has_details": False, "by_verification": {
-                "b_uuid": {"duration": 0, "status": "success",
-                           "details": None}},
-             "name": "foo", "tags": ["id-tag", "a-tag", "z-tag"]},
-            {"has_details": True, "by_verification": {
-                "a_uuid": {"duration": 4.2, "status": "fail",
-                           "details": "Some error"}},
-             "name": "spam", "tags": ["id-tag", "a-tag", "z-tag"]}]
-        self.assertEqual(tests, sorted(ins._tests, key=lambda i: i["name"]))
+    def test__init__(self):
+        vreport = report.VerificationReport(self.get_verifications())
+
+        self.assertEqual(
+            collections.OrderedDict(
+                [("foo-bar-1", {"status": "finished",
+                                "started_at": "2001-01-01 00:00:00",
+                                "finished_at": "2001-01-02 00:00:00",
+                                "tests_duration": 1.111,
+                                "tests_count": 9,
+                                "run_args": "set_name=compute",
+                                "skipped": 0,
+                                "success": 3,
+                                "unexpected_success": 2,
+                                "expected_failures": 3,
+                                "failures": 1}),
+                 ("foo-bar-2", {"status": "finished",
+                                "started_at": "2002-01-01 00:00:00",
+                                "finished_at": "2002-01-02 00:00:00",
+                                "tests_duration": 22.222,
+                                "tests_count": 99,
+                                "run_args": "set_name=full",
+                                "skipped": 0,
+                                "success": 33,
+                                "unexpected_success": 22,
+                                "expected_failures": 33,
+                                "failures": 11}),
+                 ("foo-bar-3", {"status": "finished",
+                                "started_at": "2003-01-01 00:00:00",
+                                "finished_at": "2003-01-02 00:00:00",
+                                "tests_duration": 33.333,
+                                "tests_count": 99,
+                                "run_args": "set_name=full",
+                                "skipped": 0,
+                                "success": 33,
+                                "unexpected_success": 22,
+                                "expected_failures": 33,
+                                "failures": 11})]),
+            vreport.report["verifications"])
+
+        self.assertEqual({
+            "some.test.TestCase.test_foo[id=iiiidddd;smoke]": {
+                "by_verification": {"foo-bar-1": {"details": None,
+                                                  "duration": "8",
+                                                  "status": "success"},
+                                    "foo-bar-2": {"details": None,
+                                                  "duration": "8",
+                                                  "status": "success"},
+                                    "foo-bar-3": {"details": None,
+                                                  "duration": "8",
+                                                  "status": "success"}
+                                    },
+                "name": "some.test.TestCase.test_foo",
+                "tags": ["smoke", "id"]},
+            "some.test.TestCase.test_failed": {
+                "by_verification": {"foo-bar-2": {"details": "HEEEEEEELP",
+                                                  "duration": "8",
+                                                  "status": "fail"},
+                                    "foo-bar-3": {"details": "HEEEEEEELP",
+                                                  "duration": "7",
+                                                  "status": "fail"}},
+                "name": "some.test.TestCase.test_failed",
+                "tags": []},
+            "some.test.TestCase.test_skipped": {
+                "by_verification": {
+                    "foo-bar-1": {
+                        "details": "Skipped until Bug: https://launchpad.net/"
+                                   "bugs/666 is resolved.",
+                        "duration": "0",
+                        "status": "skip"},
+                    "foo-bar-2": {
+                        "details": "Skipped until Bug: https://launchpad.net/"
+                                   "bugs/666 is resolved.",
+                        "duration": "0",
+                        "status": "skip"},
+                    "foo-bar-3": {
+                        "details": "Skipped until Bug: https://launchpad.net/"
+                                   "bugs/666 is resolved.",
+                        "duration": "0",
+                        "status": "skip"}},
+                "name": "some.test.TestCase.test_skipped",
+                "tags": []},
+            "some.test.TestCase.test_xfail": {
+                "by_verification": {
+                    "foo-bar-1": {"details": "something\n\nHEEELP",
+                                  "duration": "3",
+                                  "status": "xfail"},
+                    "foo-bar-2": {"details": "something\n\nHEEELP",
+                                  "duration": "4",
+                                  "status": "xfail"},
+                    "foo-bar-3": {"details": "something\n\nHEEELP",
+                                  "duration": "3",
+                                  "status": "xfail"}},
+                "name": "some.test.TestCase.test_xfail",
+                "tags": []}},
+            vreport.report["tests"])
 
     @mock.patch("rally.ui.report.utils")
-    @mock.patch("rally.ui.report.json.dumps", return_value="json!")
+    @mock.patch("rally.ui.report.json.dumps")
     def test_to_html(self, mock_dumps, mock_utils):
-        mock_render = mock.Mock(return_value="HTML")
-        mock_utils.get_template.return_value.render = mock_render
-        ins = self.gen_instance(runs="runs!", uuids="uuids!", tests="tests!")
-        self.assertEqual("HTML", ins.to_html())
+        mock_render = mock_utils.get_template.return_value.render
+
+        vreport = report.VerificationReport(self.get_verifications())
+
+        self.assertEqual(mock_render.return_value, vreport.to_html())
+        mock_render.assert_called_once_with(data=mock_dumps.return_value,
+                                            include_libs=False)
         mock_utils.get_template.assert_called_once_with(
             "verification/report.html")
-        mock_dumps.assert_called_once_with(
-            {"tests": "tests!", "uuids": "uuids!", "verifications": "runs!"})
-        mock_render.assert_called_once_with(data="json!", include_libs=False)
 
-    @mock.patch("rally.ui.report.json.dumps", return_value="json!")
+        self.assertEqual(1, mock_dumps.call_count)
+        args, kwargs = mock_dumps.call_args
+        self.assertTrue(not kwargs)
+        self.assertEqual(1, len(args))
+        ctx = args[0]
+        self.assertEqual({"uuids", "verifications", "tests",
+                          "show_comparison_note"},
+                         set(ctx.keys()))
+        self.assertEqual(["foo-bar-1", "foo-bar-2", "foo-bar-3"],
+                         list(ctx["uuids"]))
+        self.assertEqual(vreport.report["verifications"],
+                         ctx["verifications"])
+        self.assertTrue(ctx["show_comparison_note"])
+        self.assertEqual({
+            "some.test.TestCase.test_foo[id=iiiidddd;smoke]": {
+                "by_verification": {"foo-bar-1": {"details": None,
+                                                  "duration": "8",
+                                                  "status": "success"},
+                                    "foo-bar-2": {"details": None,
+                                                  "duration": "8 (+0.0)",
+                                                  "status": "success"},
+                                    "foo-bar-3": {"details": None,
+                                                  "duration": "8 (+0.0)",
+                                                  "status": "success"}},
+                "has_details": False,
+                "name": "some.test.TestCase.test_foo",
+                "tags": ["smoke", "id"]},
+            "some.test.TestCase.test_failed": {
+                "by_verification": {"foo-bar-2": {"details": "HEEEEEEELP",
+                                                  "duration": "8",
+                                                  "status": "fail"},
+                                    "foo-bar-3": {"details": "HEEEEEEELP",
+                                                  "duration": "7 (-1.0)",
+                                                  "status": "fail"}},
+                "has_details": True,
+                "name": "some.test.TestCase.test_failed",
+                "tags": []},
+            "some.test.TestCase.test_skipped": {
+                "by_verification": {
+                    "foo-bar-1": {"details": "Skipped until Bug: https://laun"
+                                             "chpad.net/bugs/666 is resolved.",
+                                  "duration": "",
+                                  "status": "skip"},
+                    "foo-bar-2": {"details": "Skipped until Bug: https://laun"
+                                             "chpad.net/bugs/666 is resolved.",
+                                  "duration": "",
+                                  "status": "skip"},
+                    "foo-bar-3": {"details": "Skipped until Bug: https://laun"
+                                             "chpad.net/bugs/666 is resolved.",
+                                  "duration": "",
+                                  "status": "skip"}},
+                "has_details": True,
+                "name": "some.test.TestCase.test_skipped",
+                "tags": []},
+            "some.test.TestCase.test_xfail": {
+                "by_verification": {
+                    "foo-bar-1": {"details": "something\n\nHEEELP",
+                                  "duration": "3",
+                                  "status": "xfail"},
+                    "foo-bar-2": {"details": "something\n\nHEEELP",
+                                  "duration": "4 (+1.0)",
+                                  "status": "xfail"},
+                    "foo-bar-3": {"details": "something\n\nHEEELP",
+                                  "duration": "3 (+0.0)",
+                                  "status": "xfail"}},
+                "has_details": True,
+                "name": "some.test.TestCase.test_xfail",
+                "tags": []}},
+            ctx["tests"])
+
+    @mock.patch("rally.ui.report.json.dumps")
     def test_to_json(self, mock_dumps):
-        ins = self.gen_instance(tests="tests!")
-        self.assertEqual("json!", ins.to_json())
-        mock_dumps.assert_called_once_with("tests!", indent=4)
+        obj = mock.MagicMock()
+        indent = 777
 
-    @mock.patch("rally.ui.report.csv")
-    @mock.patch("rally.ui.report.io.BytesIO")
-    def test_to_csv(self, mock_bytes_io, mock_csv):
-        ins = self.gen_instance(
-            uuids=["foo", "bar"],
-            tests=[{"name": "test-1", "tags": ["tag1", "tag2"],
-                    "has_details": False,
-                    "by_verification": {
-                        "foo": {"status": "success", "duration": 1.2}}},
-                   {"name": "test-2", "tags": ["tag3", "tag4"],
-                    "has_details": False,
-                    "by_verification": {
-                        "bar": {"status": "success", "duration": 3.4}}}])
-        mock_stream = mock.Mock()
-        mock_stream.getvalue.return_value = "CSV!"
-        mock_bytes_io.return_value.__enter__.return_value = mock_stream
-        self.assertEqual("CSV!", ins.to_csv())
-        mock_csv.writer.assert_called_once_with(mock_stream)
+        vreport = report.VerificationReport([])
+        vreport.report = obj
 
-        # Custom kwargs
-        mock_csv.writer.reset_mock()
-        self.assertEqual("CSV!", ins.to_csv(foo="bar"))
-        mock_csv.writer.assert_called_once_with(mock_stream, foo="bar")
+        self.assertEqual(mock_dumps.return_value, vreport.to_json(indent))
+
+        mock_dumps.assert_called_once_with(obj, indent=indent)
