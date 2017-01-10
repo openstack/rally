0c9d08a1b7156786bd3167943466fab1aa0bc547
Anton Studenov <astudenov@mirantis.com>
2017-01-10 11:49:05 +0300

    [Core] MetaMixin: improve implementation of default values
    * MetaMixin classes can define DEFAULT_META dict
      that will be copied to _meta in _meta_init() as per MRO priority.
    * Simplified check in _meta_is_inited()
    * DEFAULT_META_CONCATENATION defines field names and it's types that
      should be concatenated during initialization of default meta.
    
    Change-Id: Ia82828f3a78e8a63b4403002df05de1c089d8c2c

--- tests/unit/common/plugin/test_meta.py
+++ tests/unit/common/plugin/test_meta.py
@@ -24,13 +24,27 @@ class TestMetaMixinTestCase(test.TestCase):
         class Meta(meta.MetaMixin):
             pass
 
+        class SubMeta(Meta):
+            pass
+
         self.assertRaises(ReferenceError, Meta._meta_is_inited)
         self.assertFalse(Meta._meta_is_inited(raise_exc=False))
+
+        self.assertRaises(ReferenceError, SubMeta._meta_is_inited)
+        self.assertFalse(SubMeta._meta_is_inited(raise_exc=False))
 
         Meta._meta_init()
 
         self.assertTrue(Meta._meta_is_inited())
         self.assertTrue(Meta._meta_is_inited(raise_exc=False))
+
+        self.assertRaises(ReferenceError, SubMeta._meta_is_inited)
+        self.assertFalse(SubMeta._meta_is_inited(raise_exc=False))
+
+        SubMeta._meta_init()
+
+        self.assertTrue(SubMeta._meta_is_inited())
+        self.assertTrue(SubMeta._meta_is_inited(raise_exc=False))
 
     def test_meta_clear(self):
 
@@ -88,3 +102,105 @@ class TestMetaMixinTestCase(test.TestCase):
         self.assertEqual(42, Meta._meta_get("any"))
         Meta._meta_setdefault("any", 2)
         self.assertEqual(42, Meta._meta_get("any"))
+
+    def test_default_meta(self):
+
+        class Meta(meta.MetaMixin):
+            DEFAULT_META = {"foo": "bar"}
+
+        class SubMeta(Meta):
+            pass
+
+        class SubMetaWithDefault(Meta):
+            DEFAULT_META = {"foo": "spam"}
+
+        class SubSubMeta(SubMeta):
+            DEFAULT_META = {"baz": "eggs"}
+
+        Meta._meta_init()
+        SubMeta._meta_init()
+        SubMetaWithDefault._meta_init()
+        SubSubMeta._meta_init()
+
+        self.assertEqual("bar", Meta._meta_get("foo"))
+        self.assertEqual("bar", SubMeta._meta_get("foo"))
+        self.assertEqual("spam", SubMetaWithDefault._meta_get("foo"))
+        self.assertEqual("bar", SubSubMeta._meta_get("foo"))
+        self.assertEqual("eggs", SubSubMeta._meta_get("baz"))
+        self.assertIsNone(Meta._meta_get("baz"))
+        self.assertIsNone(SubMeta._meta_get("baz"))
+        self.assertIsNone(SubMetaWithDefault._meta_get("baz"))
+
+    def test_default_meta_change(self):
+
+        class Meta(meta.MetaMixin):
+            DEFAULT_META = {"foo": []}
+
+        class SubMeta(Meta):
+            pass
+
+        Meta._meta_init()
+        SubMeta._meta_init()
+
+        self.assertEqual([], Meta._meta_get("foo"))
+        self.assertEqual([], SubMeta._meta_get("foo"))
+
+        SubMeta._meta_get("foo").append("bar")
+
+        self.assertEqual([], Meta._meta_get("foo"))
+        self.assertEqual(["bar"], SubMeta._meta_get("foo"))
+
+        Meta._meta_get("foo").append("baz")
+
+        self.assertEqual(["baz"], Meta._meta_get("foo"))
+        self.assertEqual(["bar"], SubMeta._meta_get("foo"))
+
+    def test_default_meta_validators(self):
+
+        class A(meta.MetaMixin):
+            DEFAULT_META = {"validators": ["a"]}
+
+        class B(A):
+            DEFAULT_META = {"validators": ["b", "foo"]}
+
+        class C(A):
+            DEFAULT_META = {"validators": ["c", "foo"]}
+
+        class D(B, C):
+            DEFAULT_META = {"validators": ["d"]}
+
+        A._meta_init()
+        B._meta_init()
+        C._meta_init()
+        D._meta_init()
+
+        self.assertEqual(["a"], A._meta_get("validators"))
+        self.assertEqual(["a", "b", "foo"], B._meta_get("validators"))
+        self.assertEqual(["a", "c", "foo"], C._meta_get("validators"))
+        self.assertEqual(["a", "c", "foo", "b", "foo", "d"],
+                         D._meta_get("validators"))
+
+    def test_default_meta_context(self):
+
+        class A(meta.MetaMixin):
+            DEFAULT_META = {"context": {"foo": "a"}}
+
+        class B(A):
+            DEFAULT_META = {"context": {"foo": "b", "baz": "b"}}
+
+        class C(A):
+            DEFAULT_META = {"context": {"foo": "c", "spam": "c"}}
+
+        class D(B, C):
+            DEFAULT_META = {"context": {"bar": "d"}}
+
+        A._meta_init()
+        B._meta_init()
+        C._meta_init()
+        D._meta_init()
+
+        self.assertEqual({"foo": "a"}, A._meta_get("context"))
+        self.assertEqual({"foo": "b", "baz": "b"}, B._meta_get("context"))
+        self.assertEqual({"foo": "c", "spam": "c"}, C._meta_get("context"))
+        self.assertEqual({"foo": "b", "baz": "b", "spam": "c", "bar": "d"},
+                         D._meta_get("context"))
