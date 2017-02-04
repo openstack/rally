2558a22a80ae0bb01a04f77c57a9af56f7fdd98c
yuhui_inspur <yuhui@inspur.com>
2017-02-04 00:29:33 -0800

    Correct some annotation errors
    Change-Id: I71131cffe51fd0caae5fcb587e047b5a4b22ba16

--- rally/consts.py
+++ rally/consts.py
@@ -14,11 +14,11 @@
 #    under the License.
 
 """
-There is a lot of situation when we would like to work with Enum or consts.
+There is a lot of situations when we would like to work with Enum or Const.
 E.g. work around Tasks. We would like to use Enum in DB to store status of task
 and also in migration that creates DB and in business logic to set some status
-so to avoid copy paste or direct usage of enums values we create singletons
-for each enum. (e.g TaskStatus)
+so as to avoid copy paste or direct usage of enums values we create singletons
+for each enum. (e.g. TaskStatus)
 """
 
 from rally.common import utils
