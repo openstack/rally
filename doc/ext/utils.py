2be233562a7c51b122efc0ab0db51996075d2538
Andrey Kurilin <andr.kurilin@gmail.com>
2017-02-08 18:23:10 +0200

    pprint jsonschema in plugin reference
    Change-Id: I317d60391556a362b93c5a1b769d7cd44fdae5ac

--- doc/ext/utils.py
+++ doc/ext/utils.py
@@ -42,7 +42,7 @@ section = lambda title: parse_text("%s\n%s" % (title, "\"" * len(title)))[0]
 
 
 def make_definition(term, ref, descriptions):
-    """Constructs definition with reference to it"""
+    """Constructs definition with reference to it."""
     ref = ref.replace("_", "-").replace(" ", "-")
     definition = parse_text(
             ".. _%(ref)s:\n\n* *%(term)s* [ref__]\n\n__ #%(ref)s" %
@@ -55,3 +55,30 @@ def make_definition(term, ref, descriptions):
                 descr = paragraph("  %s" % descr)
             definition.append(descr)
     return definition
+
+
+def make_definitions(title, ref_prefix, terms, descriptions=None):
+    """Constructs a list of definitions with reference to them."""
+    raw_text = ["**%s**:" % title]
+    if descriptions:
+        for descr in descriptions:
+            raw_text.append(descr)
+
+    for term, definitions in terms:
+        ref = ("%s%s" % (ref_prefix, term)).lower().replace(
+            ".", "-").replace("_", "-").replace(" ", "-")
+        raw_text.append(".. _%s:" % ref)
+        raw_text.append("* *%s* [ref__]" % term)
+
+        for d in definitions:
+            d = d.strip() if d else None
+            if d:
+                if d[0] not in string.ascii_uppercase:
+                    # .capitalize() removes existing caps
+                    d = d[0].upper() + d[1:]
+                d = "\n  ".join(d.split("\n"))
+                raw_text.append("  %s" % d)
+
+        raw_text.append("__ #%s" % ref)
+
+    return parse_text("\n\n".join(raw_text) + "\n")
