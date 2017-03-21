cab46380edae30414f66b1387313249203311f05
Andrey Kurilin <andr.kurilin@gmail.com>
2017-03-21 13:19:23 +0200

    Fix nics format in servers ctx
    JSON schema of servers context allows to transmit a list of nics in two
    formats. First one is a format that novaclient expects to see. The second
    one is more user-friendly - just list of strings. Unfortunately, the second
    case was not covered by our code base.
    
    Change-Id: Ic578c66cece8a707888aa7b50cf87736421c3afc

--- tests/unit/plugins/openstack/context/nova/test_servers.py
+++ tests/unit/plugins/openstack/context/nova/test_servers.py
@@ -93,6 +93,7 @@ class ServerGeneratorTestCase(test.ScenarioTestCase):
                     "flavor": {
                         "name": "m1.tiny",
                     },
+                    "nics": ["foo", "bar"]
                 },
             },
             "admin": {
@@ -115,12 +116,11 @@ class ServerGeneratorTestCase(test.ScenarioTestCase):
         flavor_id = mock_flavor_transform.return_value
         servers_ctx_config = self.context["config"]["servers"]
         expected_auto_nic = servers_ctx_config.get("auto_assign_nic", False)
-        expected_nics = servers_ctx_config.get("nics", [])
         expected_requests = servers_ctx_config.get("servers_per_tenant", False)
         called_times = len(tenants)
         mock_calls = [mock.call(image_id, flavor_id,
                                 auto_assign_nic=expected_auto_nic,
-                                nics=expected_nics,
+                                nics=[{"net-id": "foo"}, {"net-id": "bar"}],
                                 requests=expected_requests)
                       for i in range(called_times)]
         mock_nova_scenario__boot_servers.assert_has_calls(mock_calls)
