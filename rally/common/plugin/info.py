f6b37ca7330d752d58a8a1cf20b88820d57d6953
Jon Schlueter <jschluet@redhat.com>
2017-07-14 12:56:49 -0400

    remove runtime dep on sphinx
    in rally/common/plugin/info replace sphinx prepare_docstring
    with trim function from PEP-257 reference implementation
    
    Also move Sphinx from requirements to test-requirements
    Added several unittests for PEP-257 trim function
    
    Change-Id: I9f13906cbb3ea9212a78b5bed178e15780001caf

--- rally/common/plugin/info.py
+++ rally/common/plugin/info.py
@@ -14,13 +14,46 @@
 #    under the License.
 
 import re
-
-from sphinx.util import docstrings
+import sys
 
 PARAM_OR_RETURNS_REGEX = re.compile(":(?:param|returns)")
 RETURNS_REGEX = re.compile(":returns: (?P<doc>.*)", re.S)
 PARAM_REGEX = re.compile(":param (?P<name>[\*\w]+): (?P<doc>.*?)"
                          "(?:(?=:param)|(?=:return)|(?=:raises)|\Z)", re.S)
+
+
+def trim(docstring):
+    """trim function from PEP-257"""
+    if not docstring:
+        return ""
+    # Convert tabs to spaces (following the normal Python rules)
+    # and split into a list of lines:
+    lines = docstring.expandtabs().splitlines()
+    # Determine minimum indentation (first line doesn't count):
+    indent = sys.maxsize
+    for line in lines[1:]:
+        stripped = line.lstrip()
+        if stripped:
+            indent = min(indent, len(line) - len(stripped))
+    # Remove indentation (first line is special):
+    trimmed = [lines[0].strip()]
+    if indent < sys.maxsize:
+        for line in lines[1:]:
+            trimmed.append(line[indent:].rstrip())
+    # Strip off trailing and leading blank lines:
+    while trimmed and not trimmed[-1]:
+        trimmed.pop()
+    while trimmed and not trimmed[0]:
+        trimmed.pop(0)
+
+    # Current code/unittests expects a line return at
+    # end of multiline docstrings
+    # workaround expected behavior from unittests
+    if "\n" in docstring:
+        trimmed.append("")
+
+    # Return a single string:
+    return "\n".join(trimmed)
 
 
 def reindent(string):
@@ -43,7 +76,7 @@ def parse_docstring(docstring):
     params = []
 
     if docstring:
-        docstring = "\n".join(docstrings.prepare_docstring(docstring))
+        docstring = trim(docstring)
 
         lines = docstring.split("\n", 1)
         short_description = lines[0]
@@ -61,8 +94,7 @@ def parse_docstring(docstring):
 
             if params_returns_desc:
                 params = [
-                    {"name": name,
-                     "doc": "\n".join(docstrings.prepare_docstring(doc))}
+                    {"name": name, "doc": trim(doc)}
                     for name, doc in PARAM_REGEX.findall(params_returns_desc)
                 ]
 
