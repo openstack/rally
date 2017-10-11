7b9861e9e69d05967417619dd5126509827b00c9
Andrey Kurilin <andr.kurilin@gmail.com>
2017-10-11 18:21:01 +0300

    [functional] Fix failures in DeploymentTestCase
    All the failures relates to changed format of the deployment config.
    Despite the fact that most of failures are results of wrong usage of
    format in tests itself, `rally deployment recreate` was broken for some
    time.
    
    Change-Id: Ie6afd2123d959ffeb9cdca3216c2eeadd30d656c

--- tests/functional/test_cli_deployment.py
+++ tests/functional/test_cli_deployment.py
@@ -20,22 +20,31 @@ import unittest
 from tests.functional import utils
 
 
+TEST_ENV = {
+    "OS_USERNAME": "admin",
+    "OS_PASSWORD": "admin",
+    "OS_TENANT_NAME": "admin",
+    "OS_AUTH_URL": "http://fake/",
+}
+
+
 class DeploymentTestCase(unittest.TestCase):
 
-    def setUp(self):
-        super(DeploymentTestCase, self).setUp()
-
     def test_create_fromenv_list_show(self):
+        # NOTE(andreykurilin): `rally deployment create --fromenv` is
+        #   hardcoded to OpenStack. Should be fixed as soon as the platforms
+        #   will be introduced.
         rally = utils.Rally()
-        rally.env.update(utils.TEST_ENV)
+        rally.env.update(TEST_ENV)
+
         rally("deployment create --name t_create_env --fromenv")
         self.assertIn("t_create_env", rally("deployment list"))
-        self.assertIn(utils.TEST_ENV["OS_AUTH_URL"],
+        self.assertIn(TEST_ENV["OS_AUTH_URL"],
                       rally("deployment show"))
 
     def test_create_fromfile(self):
         rally = utils.Rally()
-        rally.env.update(utils.TEST_ENV)
+        rally.env.update(TEST_ENV)
         rally("deployment create --name t_create_env --fromenv")
         with open("/tmp/.tmp.deployment", "w") as f:
             f.write(rally("deployment config"))
@@ -43,41 +52,14 @@ class DeploymentTestCase(unittest.TestCase):
               "--filename /tmp/.tmp.deployment")
         self.assertIn("t_create_file", rally("deployment list"))
 
-    @unittest.skip("It started failing due to broken launching script. "
-                   "Requires investigation.")
     def test_create_empty(self):
         rally = utils.Rally()
         rally("deployment create --name t_empty")
-        self.assertEqual("{}", rally("deployment config"))
-
-    @unittest.skip("It started failing due to broken launching script. "
-                   "Requires investigation.")
-    def test_config(self):
-        rally = utils.Rally()
-        rally.env.update(utils.TEST_ENV)
-        rally("deployment create --name t_create_env --fromenv")
-        config = json.loads(rally("deployment config"))
-        self.assertIn("creds", config)
-        self.assertIn("openstack", config["creds"])
-        oscreds = config["creds"]["openstack"]
-        self.assertEqual(utils.TEST_ENV["OS_USERNAME"],
-                         oscreds["admin"]["username"])
-        self.assertEqual(utils.TEST_ENV["OS_PASSWORD"],
-                         oscreds["admin"]["password"])
-        if "project_name" in oscreds["admin"]:
-            # keystone v3
-            self.assertEqual(utils.TEST_ENV["OS_TENANT_NAME"],
-                             oscreds["admin"]["project_name"])
-        else:
-            # keystone v2
-            self.assertEqual(utils.TEST_ENV["OS_TENANT_NAME"],
-                             oscreds["admin"]["tenant_name"])
-        self.assertEqual(utils.TEST_ENV["OS_AUTH_URL"],
-                         oscreds["auth_url"])
+        self.assertEqual("{}", rally("deployment config").strip())
 
     def test_destroy(self):
         rally = utils.Rally()
-        rally.env.update(utils.TEST_ENV)
+        rally.env.update(TEST_ENV)
         rally("deployment create --name t_create_env --fromenv")
         self.assertIn("t_create_env", rally("deployment list"))
         rally("deployment destroy")
@@ -89,18 +71,16 @@ class DeploymentTestCase(unittest.TestCase):
 
     def test_check_fail(self):
         rally = utils.Rally()
-        rally.env.update(utils.TEST_ENV)
+        rally.env.update(TEST_ENV)
         rally("deployment create --name t_create_env --fromenv")
         self.assertRaises(utils.RallyCliError, rally, "deployment check")
 
-    @unittest.skip("It started failing due to broken launching script. "
-                   "Requires investigation.")
     def test_check_debug(self):
         rally = utils.Rally()
-        rally.env.update(utils.TEST_ENV)
+        rally.env.update(TEST_ENV)
         rally("deployment create --name t_create_env --fromenv")
         config = json.loads(rally("deployment config"))
-        config["creds"]["openstack"]["admin"]["password"] = "fakepassword"
+        config["openstack"]["admin"]["password"] = "fakepassword"
         file = utils.JsonTempFile(config)
         rally("deployment create --name t_create_file_debug "
               "--filename %s" % file.filename)
@@ -115,15 +95,15 @@ class DeploymentTestCase(unittest.TestCase):
             self.assertIn(
                 "[-] Unable to authenticate for user %(username)s in"
                 " project %(tenant_name)s" %
-                {"username": utils.TEST_ENV["OS_USERNAME"],
-                 "tenant_name": utils.TEST_ENV["OS_TENANT_NAME"]},
+                {"username": TEST_ENV["OS_USERNAME"],
+                 "tenant_name": TEST_ENV["OS_TENANT_NAME"]},
                 str(e))
             self.assertIn(
                 "AuthenticationFailed: Failed to authenticate to %(auth_url)s"
                 " for user '%(username)s' in project '%(tenant_name)s'" %
-                {"auth_url": utils.TEST_ENV["OS_AUTH_URL"],
-                 "username": utils.TEST_ENV["OS_USERNAME"],
-                 "tenant_name": utils.TEST_ENV["OS_TENANT_NAME"]},
+                {"auth_url": TEST_ENV["OS_AUTH_URL"],
+                 "username": TEST_ENV["OS_USERNAME"],
+                 "tenant_name": TEST_ENV["OS_TENANT_NAME"]},
                 str(e))
         else:
             self.fail("rally deployment fails to raise error for wrong"
@@ -131,19 +111,17 @@ class DeploymentTestCase(unittest.TestCase):
 
     def test_recreate(self):
         rally = utils.Rally()
-        rally.env.update(utils.TEST_ENV)
+        rally.env.update(TEST_ENV)
         rally("deployment create --name t_create_env --fromenv")
         rally("deployment recreate --deployment t_create_env")
         self.assertIn("t_create_env", rally("deployment list"))
 
-    @unittest.skip("It started failing due to broken launching script. "
-                   "Requires investigation.")
     def test_recreate_from_file(self):
         rally = utils.Rally()
-        rally.env.update(utils.TEST_ENV)
+        rally.env.update(TEST_ENV)
         rally("deployment create --name t_create_env --fromenv")
         config = json.loads(rally("deployment config"))
-        config["creds"]["openstack"]["auth_url"] = "http://foo/"
+        config["openstack"]["auth_url"] = "http://foo/"
         file = utils.JsonTempFile(config)
         rally("deployment recreate --deployment t_create_env "
               "--filename %s" % file.filename)
@@ -154,7 +132,7 @@ class DeploymentTestCase(unittest.TestCase):
 
     def test_use(self):
         rally = utils.Rally()
-        rally.env.update(utils.TEST_ENV)
+        rally.env.update(TEST_ENV)
         output = rally(
             "deployment create --name t_create_env1 --fromenv")
         uuid = re.search(r"Using deployment: (?P<uuid>[0-9a-f\-]{36})",
@@ -164,3 +142,27 @@ class DeploymentTestCase(unittest.TestCase):
         current_deployment = utils.get_global("RALLY_DEPLOYMENT",
                                               rally.env)
         self.assertEqual(uuid, current_deployment)
+
+    # TODO(andreykurilin): Do not forget to move thes tests while splitting
+    #   rally to main framework and openstack plugins
+
+    def test_create_from_env_openstack_deployment(self):
+        rally = utils.Rally()
+        rally.env.update(TEST_ENV)
+        rally("deployment create --name t_create_env --fromenv")
+        config = json.loads(rally("deployment config"))
+        self.assertIn("openstack", config)
+        self.assertEqual(TEST_ENV["OS_USERNAME"],
+                         config["openstack"]["admin"]["username"])
+        self.assertEqual(TEST_ENV["OS_PASSWORD"],
+                         config["openstack"]["admin"]["password"])
+        if "project_name" in config["openstack"]["admin"]:
+            # keystone v3
+            self.assertEqual(TEST_ENV["OS_TENANT_NAME"],
+                             config["openstack"]["admin"]["project_name"])
+        else:
+            # keystone v2
+            self.assertEqual(TEST_ENV["OS_TENANT_NAME"],
+                             config["openstack"]["admin"]["tenant_name"])
+        self.assertEqual(TEST_ENV["OS_AUTH_URL"],
+                         config["openstack"]["auth_url"])
