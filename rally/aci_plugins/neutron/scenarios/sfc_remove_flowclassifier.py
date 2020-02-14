e294ece276db8eb33bd0d5c1e57ac334dc8d3b54
mdsufair <md.sufair@oneconvergence.com>
2020-02-14 03:24:59 -0800

    vCPE support for aci-openstack setup

---
+++ rally/aci_plugins/neutron/scenarios/sfc_remove_flowclassifier.py
@@ -0,0 +1,104 @@
+from rally import consts
+from rally import exceptions
+from rally.task import utils
+from rally.task import atomic
+from rally.task import validation
+from rally.common import validation
+from rally.aci_plugins import vcpe_utils
+from rally.plugins.openstack import scenario
+from rally.plugins.openstack.scenarios.nova import utils as nova_utils
+from rally.plugins.openstack.scenarios.neutron import utils as neutron_utils
+
+@validation.add("required_services", services=[consts.Service.NOVA, consts.Service.NEUTRON])
+@validation.add("required_platform", platform="openstack", users=True)
+@scenario.configure(name="ScenarioPlugin.sfc_remove_flowclassifier", context={"cleanup@openstack": ["nova", "neutron"],
+                             "keypair@openstack": {},
+                             "allow_ssh@openstack": None}, platform="openstack")
+
+class SFCRemoveFlowclassifier(vcpe_utils.vCPEScenario, neutron_utils.NeutronScenario, nova_utils.NovaScenario, scenario.OpenStackScenario):
+
+    def run(self, src_cidr, dest_cidr, image, flavor, public_net, username, password):
+        
+        public_network = self.clients("neutron").show_network(public_net)        
+        secgroup = self.context.get("user", {}).get("secgroup")
+        key_name=self.context["user"]["keypair"]["name"]
+
+        net1, sub1 = self._create_network_and_subnets({},{"cidr": src_cidr}, 1, None)
+        net2, sub2 = self._create_network_and_subnets({},{"cidr": dest_cidr}, 1, None)
+        left, sub3 = self._create_network_and_subnets({},{"cidr": "1.1.0.0/24", 'host_routes': [{'destination': src_cidr, 'nexthop': '1.1.0.1'}]}, 1, None)
+        right, sub4 = self._create_network_and_subnets({},{"cidr": "2.2.0.0/24", 'host_routes': [{'destination': '0.0.0.0/1', 'nexthop': '2.2.0.1'}, {'destination': '128.0.0.0/1', 'nexthop': '2.2.0.1'}]}, 1, None)
+        test_net, sub5 = self._create_network_and_subnets({},{"cidr": '192.168.0.0/24'}, 1, None)
+
+        router = self._create_router({}, False)
+        self._add_interface_router(sub1[0].get("subnet"), router.get("router"))
+        self._add_interface_router(sub2[0].get("subnet"), router.get("router"))
+        self._add_interface_router(sub3[0].get("subnet"), router.get("router"))
+        self._add_interface_router(sub4[0].get("subnet"), router.get("router"))
+        self._add_interface_router(sub5[0].get("subnet"), router.get("router"))
+
+        net1_id = net1.get('network', {}).get('id')
+        net2_id = net2.get('network', {}).get('id')
+        testnet_id = test_net.get('network', {}).get('id')
+
+        port_create_args = {}
+        port_create_args["security_groups"] = [secgroup.get('id')]
+        p1 = self._create_port(public_network, port_create_args)
+        p1_id = p1.get('port', {}).get('id')
+        psrc = self._create_port(net1, port_create_args)
+        psrc_id = psrc.get('port', {}).get('id')
+        nics = [{"port-id": p1_id},{"port-id": psrc_id}]
+        kwargs = {}
+        kwargs.update({'nics': nics})
+        kwargs.update({'key_name': key_name})
+        src_vm = self._boot_server(image, flavor, False, **kwargs)
+        
+        pdest = self._create_port(net2, port_create_args)
+        pdest_id = pdest.get('port', {}).get('id')
+        nics = [{"port-id": pdest_id}]
+        kwargs = {}
+        kwargs.update({'nics': nics})
+        dest_vm = self._boot_server(image, flavor, False, **kwargs)
+        
+        port_create_args = {}
+        port_create_args.update({"port_security_enabled": "false"})
+        pin = self._create_port(left, port_create_args)
+        pout = self._create_port(right, port_create_args)
+        kwargs = {}
+        pin_id = pin.get('port', {}).get('id')
+        pout_id = pout.get('port', {}).get('id')
+        nics = [{"port-id": pin_id}, {"port-id": pout_id}]
+        kwargs.update({'nics': nics})
+        kwargs.update({'key_name': key_name})
+        service_vm = self._boot_server(image, flavor, False, **kwargs)
+        self.sleep_between(30, 40) 
+
+        fip = p1.get('port', {}).get('fixed_ips')[0].get('ip_address')
+        pdest_add = pdest.get('port', {}).get('fixed_ips')[0].get('ip_address')
+        command = {
+                    "interpreter": "/bin/sh",
+                    "script_inline": "ping -c 10 " + pdest_add 
+                } 
+        
+        pp = self._create_port_pair(pin, pout)
+        ppg = self._create_port_pair_group([pp])
+        fc1 = self._create_flow_classifier(src_cidr, '192.168.0.0/24', net1_id, testnet_id)
+        fc2 = self._create_flow_classifier(src_cidr, dest_cidr, net1_id, net2_id)
+        pc = self._create_port_chain([ppg], [fc1, fc2])
+        self.sleep_between(30, 40)
+       
+        print "\nTraffic verification with multiple flow classifiers\n"
+        self._remote_command(username, password, fip, command, src_vm)
+        
+        print "Removing a flow classifier from the chain..."
+        self._update_port_chain(pc, [ppg], [fc1])
+        self.sleep_between(30, 40)
+
+        print "\nTraffic verification after removing a flow classifier\n"
+        self._remote_command(username, password, fip, command, src_vm)
+
+        self._delete_port_chain(pc)
+        self._delete_port_pair_group(ppg)
+        self._delete_flow_classifier(fc1)
+        self._delete_flow_classifier(fc2)
+        self._delete_port_pair(pp)
+
--- rally/aci_plugins/vcpe_utils.py
+++ rally/aci_plugins/vcpe_utils.py
@@ -2,9 +2,71 @@ from rally import exceptions
 from rally.plugins.openstack import scenario
 from rally.task import atomic
 from rally.task import utils
+from rally.common import cfg
+import json
+from rally.common import logging
+from rally.common import sshutils
+from rally.plugins.openstack.scenarios.vm import utils as vm_utils
 
-class vCPEScenario(scenario.OpenStackScenario):
-    
+LOG = logging.getLogger(__name__)
+CONF = cfg.CONF
+
+class vCPEScenario(vm_utils.VMScenario, scenario.OpenStackScenario):
+
+    def _remote_command(self, username, password, fip, command, server, **kwargs):
+
+        port=22
+        wait_for_ping=True
+        max_log_length=None
+
+        try:
+            if wait_for_ping:
+                self._wait_for_ping(fip)
+
+            code, out, err = self._run_command(
+                fip, port, username, password, command=command)
+
+            print 'LOGS: out: ', out
+
+            text_area_output = ["StdErr: %s" % (err or "(none)"),
+                                "StdOut:"]
+
+            if code:
+                print exceptions.ScriptError(
+                    "Error running command %(command)s. "
+                    "Error %(code)s: %(error)s" % {
+                        "command": command, "code": code, "error": err})
+                print "\nRemote command execution failed"
+                print "------------------------------"
+            else:
+                print "Remote command execution passed"
+                print "------------------------------"
+            # Let's try to load output data
+            try:
+                data = json.loads(out)
+                if not isinstance(data, dict):
+                    raise ValueError
+            except ValueError:
+                # It's not a JSON, probably it's 'script_inline' result
+                data = []
+        except (exceptions.TimeoutException,
+                exceptions.SSHTimeout):
+            console_logs = self._get_server_console_output(server,
+                                                           max_log_length)
+            LOG.debug("VM console logs:\n%s" % console_logs)
+            raise
+
+        if isinstance(data, dict) and set(data) == {"additive", "complete"}:
+            for chart_type, charts in data.items():
+                for chart in charts:
+                    self.add_output(**{chart_type: chart})
+        else:
+            # it's a dict with several unknown lines
+            text_area_output.extend(out.split("\n"))
+            self.add_output(complete={"title": "Script Output",
+                                      "chart_plugin": "TextArea",
+                                      "data": text_area_output})
+
     
     @atomic.action_timer("neutron.create_port_pair")
     def _create_port_pair(self, port1, port2, **port_pair_create_args):
@@ -47,7 +109,7 @@ class vCPEScenario(scenario.OpenStackScenario):
     def _create_port_pair_group(self, portpair, **port_pair_group_create_args):
 
     	port_pair_group_create_args = {}
-        port_pair_group_create_args["port_pairs"] = [portpair["port_pair"]["id"]]
+        port_pair_group_create_args["port_pairs"] = [pp["port_pair"]["id"] for pp in portpair]
         port_pair_group_create_args["name"] = self.generate_random_name()
         return self.clients("neutron").create_sfc_port_pair_group({"port_pair_group": port_pair_group_create_args})
 
@@ -65,8 +127,9 @@ class vCPEScenario(scenario.OpenStackScenario):
 
 
     @atomic.action_timer("neutron.update_port_pair_group")
-    def _update_port_pair_group(self, port_pair_group, **port_pair_group_update_args):
-
+    def _update_port_pair_group(self, port_pair_group, portpair, **port_pair_group_update_args):
+        
+        port_pair_group_update_args["port_pairs"] = [pp["port_pair"]["id"] for pp in portpair]
         port_pair_group_update_args["name"] = self.generate_random_name()
         body = {"port_pair_group": port_pair_group_update_args}
         return self.clients("neutron").update_sfc_port_pair_group(port_pair_group["port_pair_group"]["id"], body)
@@ -80,12 +143,12 @@ class vCPEScenario(scenario.OpenStackScenario):
 
 
     @atomic.action_timer("neutron.create_flow_classifier")
-    def _create_flow_classifier(self, source_ip, dest_ip, l7_para, **flow_classifier_create_args):
+    def _create_flow_classifier(self, source_ip, dest_ip, log_src_net, log_dest_net, **flow_classifier_create_args):
 
     	flow_classifier_create_args = {}
         flow_classifier_create_args["source_ip_prefix"] = source_ip
         flow_classifier_create_args["destination_ip_prefix"] = dest_ip
-        flow_classifier_create_args["l7_parameters"] = l7_para
+        flow_classifier_create_args["l7_parameters"] = {"logical_source_network": log_src_net, "logical_destination_network": log_dest_net}
         flow_classifier_create_args["name"] = self.generate_random_name()
         return self.clients("neutron").create_sfc_flow_classifier({"flow_classifier": flow_classifier_create_args})
 
@@ -120,8 +183,8 @@ class vCPEScenario(scenario.OpenStackScenario):
     def _create_port_chain(self, portpairgroup, flowclassifier, **port_chain_create_args):
 
     	port_chain_create_args = {}
-        port_chain_create_args["port_pair_groups"] = [portpairgroup["port_pair_group"]["id"]]
-        port_chain_create_args["flow_classifiers"] = [flowclassifier["flow_classifier"]["id"]]
+        port_chain_create_args["port_pair_groups"] = [ppg["port_pair_group"]["id"] for ppg in portpairgroup]
+        port_chain_create_args["flow_classifiers"] = [fc["flow_classifier"]["id"] for fc in flowclassifier]
         port_chain_create_args["name"] = self.generate_random_name()
         return self.clients("neutron").create_sfc_port_chain({"port_chain": port_chain_create_args})
 
@@ -139,8 +202,10 @@ class vCPEScenario(scenario.OpenStackScenario):
 
 
     @atomic.action_timer("neutron.update_port_chain")
-    def _update_port_chain(self, port_chain, **port_chain_update_args):
+    def _update_port_chain(self, port_chain, portpairgroup, flowclassifier, **port_chain_update_args):
 
+        port_chain_update_args["port_pair_groups"] = [ppg["port_pair_group"]["id"] for ppg in portpairgroup]
+        port_chain_update_args["flow_classifiers"] = [fc["flow_classifier"]["id"] for fc in flowclassifier]
         port_chain_update_args["name"] = self.generate_random_name()
         body = {"port_chain": port_chain_update_args}
         return self.clients("neutron").update_sfc_port_chain(port_chain["port_chain"]["id"], body)
@@ -150,4 +215,36 @@ class vCPEScenario(scenario.OpenStackScenario):
     def _delete_port_chain(self, port_chain):
 
         self.clients("neutron").delete_sfc_port_chain(port_chain["port_chain"]["id"])
+
+    @atomic.action_timer("neutron.delete_trunk")
+    def _delete_trunk(self, trunk_port):
+
+        self.clients("neutron").delete_trunk(trunk_port["trunk"]["id"])
+
+    @atomic.action_timer("neutron.create_trunk")
+    def _create_trunk(self, trunk_payload):
+
+        trunk_payload["name"] = self.generate_random_name()
+        return self.clients("neutron").create_trunk({"trunk": trunk_payload})
+
+    @atomic.action_timer("neutron.list_trunks")
+    def _list_trunks(self, **kwargs):
+
+        return self.clients("neutron").list_trunks(**kwargs)["trunks"]
+
+    @atomic.action_timer("neutron.list_ports_by_device_id")
+    def _list_ports_by_device_id(self, device_id):
+
+        return self.clients("neutron").list_ports(device_id=device_id)
+
+    @atomic.action_timer("neutron.list_subports_by_trunk")
+    def _list_subports_by_trunk(self, trunk_id):
+
+        return self.clients("neutron").trunk_get_subports(trunk_id)
+
+    @atomic.action_timer("neutron._add_subports_to_trunk")
+    def _add_subports_to_trunk(self, trunk_id, subports):
+
+        return self.clients("neutron").trunk_add_subports(
+            trunk_id, {"sub_ports": subports})
 
