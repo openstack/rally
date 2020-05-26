d2ed73238b5f14c53426a8cb5a971bae5f91aca9
mdsufair <md.sufair@oneconvergence.com>
2020-05-26 00:29:48 -0700

    vCPE support for aci-openstack setup

--- rally/aci_plugins/setup_env/generate_args_json_file.sh
+++ rally/aci_plugins/setup_env/generate_args_json_file.sh
@@ -8,11 +8,18 @@ r5=$(openstack image list | grep 'OpenWRTSI1' | cut -d '|' -f2)
 r6=$(openstack image list | grep 'OpenWRTSI2' | cut -d '|' -f2)
 r7=$(openstack image list | grep 'OpenWRTSI3' | cut -d '|' -f2)
 r8=$(openstack image list | grep 'cirros' | cut -d '|' -f2)
-sed "s/p1/$r1/g" template.json > args.json
-sed -i -e "s/p2/$r2/g" args.json
-sed -i -e "s/p3/$r3/g" args.json
-sed -i -e "s/p4/$r4/g" args.json
-sed -i -e "s/p5/$r5/g" args.json
-sed -i -e "s/p6/$r6/g" args.json
-sed -i -e "s/p7/$r7/g" args.json
-sed -i -e "s/p8/$r8/g" args.json
+sed "s/par1/$r1/g" template.json > args.json
+sed -i -e "s/par2/$r2/g" args.json
+sed -i -e "s/par3/$r3/g" args.json
+sed -i -e "s/par4/$r4/g" args.json
+sed -i -e "s/par5/$r5/g" args.json
+sed -i -e "s/par6/$r6/g" args.json
+sed -i -e "s/par7/$r7/g" args.json
+sed -i -e "s/par8/$r8/g" args.json
+i=1
+while IFS="=" read -r key value;do
+	s=$value
+        sed -i -e "s/argu$i/$s/g" args.json
+        i=$((i+1));
+done < conf.txt
+
